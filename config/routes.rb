4bed00489b0e0cc38558535951a1586a412292a7
Rhian Lewis <rhian.lewis@unboxed.co>
2020-04-15 12:12:07 +0100

    added layouts, government styling for login page and permissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get 'home/index'
+
+  root to: "home#index"
 end
--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe "Homes", type: :request do
 
   describe "GET /index" do
-    it "returns http success" do
+    it "should redirect to login page" do
       get "/home/index"
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to("/users/sign_in")
     end
   end
 
