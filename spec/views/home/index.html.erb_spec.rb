9f33bdfaf25e1c2356e2ee53ba46ba0ee7e5d89e
Rhian Lewis <rhian.lewis@unboxed.co>
2020-04-15 15:55:41 +0100

    added spec for home page view

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when the user is signed in' do
+    it "should allow signed-in user to view content" do
+      #We can create tests for this once we figure out if the different users need different pages
+    end
+  end
+
+  context 'when the user is not signed in' do
+    it "should not allow anonymous user to view content" do
+      expect(rendered).not_to match(/Welcome/)
+    end
+  end
 end
