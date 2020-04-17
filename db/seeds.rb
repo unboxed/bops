a34524592c8ce6a1c4a4fdefbd77eb0a4294258b
Evangelos Giataganas <e.giataganas@gmail.com>
2020-04-17 17:57:55 +0100

    Add sample data for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+User.find_or_create_by!(email: "admin@example.com") do |user|
+  user.name = "Admin"
+
+  user.password = "password"
+  user.password_confirmation = "password"
+
+  user.role = :admin
+end
