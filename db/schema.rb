2ec14aa48d855b6448ddb9d6bae2f6fe5664df70
Evangelos Giataganas <e.giataganas@gmail.com>
2020-04-17 16:36:35 +0100

    Enable postgis extension

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_173900) do
+ActiveRecord::Schema.define(version: 2020_04_17_163507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
