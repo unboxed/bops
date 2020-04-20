6af2efbc1b7a3bdd80f21d77df6f3bb6188189dc
Evangelos Giataganas <e.giataganas@gmail.com>
2020-04-20 19:43:01 +0100

    Move database migration as a manual task

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,28 +13,6 @@ echo "Connected to postgres..."
 echo "Bundling gems"
 bundle check || bundle install
 
-if [ -z ${DATABASE_URL+x} ]; then
-  echo "Skipping database setup"
-else
-  echo "Checking database setup is up to date"
-  # Rails will throw an error if no database exists"
-  if bundle exec rails db:migrate:status &> /dev/null; then
-    echo "Database found"
-    if bundle exec rails db:migrate:status | grep "^\s*down"; then
-      echo "Running db:migrate"
-      bundle exec rails db:migrate
-    else
-      echo "No pending migrations found."
-    fi
-  else
-    echo "Runs db:create, db:schema:load and db:seed."
-    bundle exec rails db:setup
-    echo "Create sample  the database."
-    bundle exec rails create_sample_data
-  fi
-  echo "Finished database setup."
-fi
-
 if [ "$2" == 'server' ]; then
   if [ -f /app/tmp/pids/server.pid ]; then
     echo 'Deleting old server pid file ...'
