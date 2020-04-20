a85531544b6439b5fdfc7ec1cde89739aad1d21f
Rhian Lewis <rhianatwork@gmail.com>
2020-04-20 19:08:37 +0100

    improved spec by adding let! blocks for user creation

--- spec/system/log_in_spec.rb
+++ spec/system/log_in_spec.rb
@@ -3,6 +3,10 @@
 require "rails_helper"
 
 RSpec.feature "Home page renders correctly", type: :system do
+  let!(:assessor) { create(:user, :assessor) }
+  let!(:reviewer) { create(:user, :reviewer) }
+  let!(:admin) { create(:user, :admin) }
+
   scenario "Home page redirects to login" do
     visit "/"
     expect(page).to have_text("Email")
@@ -10,72 +14,53 @@ RSpec.feature "Home page renders correctly", type: :system do
   end
 
   scenario "User can log in with valid credentials" do
-    assessor = create(:user, :assessor)
     visit "/"
     sign_in(assessor)
     expect(page).to have_text("Welcome")
   end
 
   scenario "User cannot log in with invalid credentials" do
-    assessor = create(:user, :assessor, password: "xxxxxxxxxx")
+    invalid_user = create(:user, :assessor, password: "xxxxxxxxxx")
     visit "/"
-    fill_in("user[email]", with: assessor.email)
-    fill_in("user[password]", with: assessor.password)
+    fill_in("user[email]", with: invalid_user.email)
+    fill_in("user[password]", with: invalid_user.password)
     expect(page).to have_text("Email")
     expect(page).not_to have_text("Welcome")
   end
 
-  context "as an assessor" do
-    before do
-      @assessor = create(:user, :assessor)
-    end
+  scenario "Planning Officer can see name on welcome screen" do
+    visit "/"
+    sign_in(assessor)
+    expect(page).to have_text(assessor.name)
+  end
 
-    scenario "Planning Officer can see name on welcome screen" do
+  scenario "Planning Officer has correct permission level" do
       visit "/"
-      sign_in(@assessor)
-      expect(page).to have_text(@assessor.name)
-    end
-
-    scenario "Planning Officer has correct permission level" do
-      visit "/"
-      sign_in(@assessor)
+      sign_in(assessor)
       expect(page).to have_text("Anyone who is logged in")
     end
+
+  scenario "Planning Manager can see name on welcome screen" do
+    visit "/"
+    sign_in(reviewer)
+    expect(page).to have_text(reviewer.name)
   end
 
-  context "as a reviewer" do
-    before do
-      @reviewer = create(:user, :reviewer)
-    end
-
-    scenario "Planning Manager can see name on welcome screen" do
-      visit "/"
-      sign_in(@reviewer)
-      expect(page).to have_text(@reviewer.name)
-    end
-
-    scenario "Planning Manager has correct permission level" do
-      visit "/"
-      sign_in(@reviewer)
-      expect(page).to have_text("logged in as a Planning Manager")
-    end
+  scenario "Planning Manager has correct permission level" do
+    visit "/"
+    sign_in(reviewer)
+    expect(page).to have_text("logged in as a Planning Manager")
   end
 
-  context "as an admin user" do
-    before do
-      @admin = create(:user, :admin)
-    end
+  scenario "Admin can see name on welcome screen" do
+    visit "/"
+    sign_in(admin)
+    expect(page).to have_text(admin.name)
+  end
 
-    scenario "Admin can see name on welcome screen" do
-        visit "/"
-        sign_in(@admin)
-        expect(page).to have_text(@admin.name)
-      end
-
-    scenario "Admin has correct permission level" do
-      visit "/"
-      sign_in(@admin)
-      expect(page).to have_text("logged in as an Admin")
-    end
+  scenario "Admin has correct permission level" do
+    visit "/"
+    sign_in(admin)
+    expect(page).to have_text("logged in as an Admin")
   end
 end
