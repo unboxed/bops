741c2d4e3cd58026d54d54e9388c3e3ae97523b1
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-04-23 19:03:00 +0100

    Add helper methods for setting decisions
    The idea is to use these instead of setting the status on its own.
    
    Also:
      - Include Rails time helpers in all specs

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -5,4 +5,12 @@ class Decision < ApplicationRecord
   belongs_to :user
 
   enum status: { pending: 0, granted: 1, refused: 2 }
+
+  def mark_granted
+    update(status: :granted, decided_at: Time.current)
+  end
+
+  def mark_refused
+    update(status: :refused, decided_at: Time.current)
+  end
 end
