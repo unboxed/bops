79bcffc7be2c6e13fe4bd0b571378ec76e871016
Rhian Lewis <rhianatwork@gmail.com>
2020-04-29 13:03:02 +0100

    updated seeds to include Agent and Applicant

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,17 @@ User.find_or_create_by!(email: "admin@example.com") do |user|
 
   user.role = :admin
 end
+
+Agent.find_or_create_by!(email: "agent@example.com") do |agent|
+  agent.name = "Jane Agent"
+
+  agent.phone = "0794876987"
+end
+
+Applicant.find_or_create_by!(email: "applicant@example.com") do |applicant|
+  applicant.name = "Jason Agent"
+
+  applicant.phone = "0794111111"
+end
+
+
