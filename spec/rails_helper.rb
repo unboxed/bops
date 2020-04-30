65219a1a8c573783d8bb854a46f8fe864353b761
Evangelos Giataganas <e.giataganas@gmail.com>
2020-04-30 15:00:02 +0100

    Change home page to planning_applications index

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,4 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
-
-  config.include AuthenticationHelper
 end
--- spec/system/log_in_spec.rb
+++ spec/system/log_in_spec.rb
@@ -2,70 +2,62 @@
 
 require "rails_helper"
 
-RSpec.feature "Home page renders correctly", type: :system do
-  let!(:assessor) { create(:user, :assessor) }
-  let!(:reviewer) { create(:user, :reviewer) }
-  let!(:admin) { create(:user, :admin) }
+RSpec.feature "Sign in", type: :system do
+  let(:admin) { create(:user, :admin) }
 
   scenario "Home page redirects to login" do
     visit "/"
     expect(page).to have_text("Email")
-    expect(page).not_to have_text("Welcome")
-  end
-
-  scenario "User can log in with valid credentials" do
-    sign_in(assessor)
-    expect(page).to have_text("Welcome")
+    expect(page).not_to have_text("Your fast track applications")
   end
 
   scenario "User cannot log in with invalid credentials" do
-    invalid_user = create(:user, :assessor, password: "xxxxxxxxxx")
     visit "/"
-    fill_in("user[email]", with: invalid_user.email)
-    fill_in("user[password]", with: invalid_user.password)
+    fill_in("user[email]", with: admin.email)
+    fill_in("user[password]", with: "invalid_password")
+    click_button('Log in')
+
     expect(page).to have_text("Email")
     expect(page).not_to have_text("Welcome")
   end
 
-  context "as an assessor" do
+  context "users with valid credentials" do
+    context "as an assessor" do
+      let(:assessor) { create(:user, :assessor) }
       before do
         sign_in(assessor)
+        visit "/"
       end
 
-      scenario "Planning Officer can see name on welcome screen" do
+      scenario "can see their name and role" do
         expect(page).to have_text(assessor.name)
-      end
-
-      scenario "Planning Officer has correct permission level" do
-        expect(page).to have_text("Anyone who is logged in")
+        expect(page).to have_text(assessor.role.capitalize)
       end
     end
 
-  context "as a reviewer" do
-    before do
-      sign_in(reviewer)
+    context "as a reviewer" do
+      let(:reviewer) { create(:user, :reviewer) }
+      before do
+        sign_in(reviewer)
+        visit "/"
+      end
+
+      scenario "can see their name and role" do
+        expect(page).to have_text(reviewer.name)
+        expect(page).to have_text(reviewer.role.capitalize)
+      end
     end
 
-    scenario "Planning Manager can see name on welcome screen" do
-      expect(page).to have_text(reviewer.name)
-    end
-
-    scenario "Planning Manager has correct permission level" do
-      expect(page).to have_text("logged in as a Planning Manager")
-    end
-  end
-
-  context "as an admin" do
+    context "as an admin" do
       before do
         sign_in(admin)
+        visit "/"
       end
 
-      scenario "Admin can see name on welcome screen" do
+      scenario "see can see their name and role" do
         expect(page).to have_text(admin.name)
-      end
-
-      scenario "Admin has correct permission level" do
-        expect(page).to have_text("logged in as an Admin")
+        expect(page).to have_text(admin.role.capitalize)
       end
     end
+  end
 end
