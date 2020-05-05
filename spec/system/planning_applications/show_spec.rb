5625a455ef3ee1fbd32b2eaee69016895b18a04d
Rhian Lewis <rhianatwork@gmail.com>
2020-05-05 08:51:06 +0100

    updated tests to accommodate new show layout

--- spec/system/planning_applications/show_spec.rb
+++ spec/system/planning_applications/show_spec.rb
@@ -36,69 +36,69 @@ RSpec.feature "Planning Application show page", type: :system do
     end
 
     scenario "Status is correct" do
-      within(".govuk-grid-column-two-thirds") do
+      within(".govuk-grid-column-two-thirds.application") do
          first('.govuk-accordion').click_button('Open all')
          expect(page).to have_text("Ready For Assessment")
        end
     end
 
     scenario "Submission date is correct" do
-      within(".govuk-grid-column-two-thirds") do
+      within(".govuk-grid-column-two-thirds.application") do
       first('.govuk-accordion').click_button('Open all')
       expect(page).to have_text(Time.zone.today.to_formatted_s(:long))
      end
     end
 
     scenario "Applicant name is correct" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button("Open all")
       end
       expect(page).to have_text(planning_application.applicant.name)
     end
 
     scenario "Applicant phone is correct" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button("Open all")
       end
       expect(page).to have_text(planning_application.applicant.phone)
     end
 
     scenario "Applicant email is correct" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button('Open all')
       end
       expect(page).to have_text(planning_application.applicant.email)
     end
 
     scenario "Agent name is correct" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button('Open all')
       end
       expect(page).to have_text(planning_application.agent.name)
     end
 
     scenario "Agent phone is correct" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button('Open all')
       end
       expect(page).to have_text(planning_application.agent.phone)
     end
 
     scenario "Agent email is correct" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button('Open all')
       end
       expect(page).to have_text(planning_application.agent.email)
     end
 
     scenario "Application information accordion is minimised by default" do
-      within(".govuk-grid-column-two-thirds") do
+      within(".govuk-grid-column-two-thirds.application") do
         expect(page).to have_button("Open all")
       end
     end
 
     scenario "Supporting information accordion is minimised by default" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         expect(page).to have_button("Open all")
       end
     end
@@ -112,28 +112,28 @@ RSpec.feature "Planning Application show page", type: :system do
     end
 
     scenario "Constraints section is visible" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button('Open all')
       end
       expect(page).to have_text("Constraints")
     end
 
     scenario "Property history section is visible" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button('Open all')
       end
       expect(page).to have_text("Property history")
     end
 
     scenario "Consultation section is visible" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button('Open all')
       end
       expect(page).to have_text("Consultation")
     end
 
     scenario "Site address is visible in Supporting information accordion" do
-      within(".govuk-grid-column-one-third") do
+      within(".govuk-grid-column-one-third.supporting") do
         click_button('Open all')
       end
       expect(page).to have_text(planning_application.site.address_1)
