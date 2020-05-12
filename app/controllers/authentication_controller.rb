5ae1be19d91d3dc0d92bc8b0e6d9f50e80d575ca
Evangelos Giataganas <e.giataganas@gmail.com>
2020-05-12 16:40:40 +0100

    Add AuthenticationController for device and pundit

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class AuthenticationController < ApplicationController
+  include Pundit
+
+  before_action :authenticate_user!
+
+  # rubocop:disable Rails/LexicallyScopedActionFilter
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+  # rubocop:enable Rails/LexicallyScopedActionFilter
+
+  rescue_from ActionController::InvalidAuthenticityToken, with: :please_retry
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def pundit_redirect_url
+    request.referer || root_path
+  end
+
+  def user_not_authorized(_e, message = I18n.t("user_not_authorized"))
+    respond_to do |format|
+      format.html { redirect_to pundit_redirect_url, alert: message }
+      format.json { render json: [message], status: :unauthorized }
+    end
+  end
+
+  private
+
+
+  def please_retry(exception)
+    reset_session
+
+    redirect_to request.referer
+  end
+end
--- app/controllers/planning_applications_controller.rb
+++ app/controllers/planning_applications_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
-class PlanningApplicationsController < ApplicationController
+class PlanningApplicationsController < AuthenticationController
   def index
     @planning_applications = PlanningApplication.all
+
+    policy_scope(@planning_applications)
   end
 
   def show
@@ -10,5 +12,7 @@ class PlanningApplicationsController < ApplicationController
     @site = @planning_application.site
     @agent = @planning_application.agent if @planning_application.agent
     @applicant = @planning_application.applicant
+
+    authorize(@planning_application)
   end
 end
