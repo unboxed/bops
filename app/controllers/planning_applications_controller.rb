b19574a3ea1c8bd20aa1ab883dbf566dcb9c93d4
Evangelos Giataganas <e.giataganas@gmail.com>
2020-05-13 10:59:42 +0100

    Use authorize and policy_scope so that we work with the return values

--- app/controllers/planning_applications_controller.rb
+++ app/controllers/planning_applications_controller.rb
@@ -2,17 +2,13 @@
 
 class PlanningApplicationsController < AuthenticationController
   def index
-    @planning_applications = PlanningApplication.all
-
-    policy_scope(@planning_applications)
+    @planning_applications = policy_scope(PlanningApplication.all)
   end
 
   def show
-    @planning_application = PlanningApplication.find(params[:id])
+    @planning_application = authorize(PlanningApplication.find(params[:id]))
     @site = @planning_application.site
     @agent = @planning_application.agent if @planning_application.agent
     @applicant = @planning_application.applicant
-
-    authorize(@planning_application)
   end
 end
