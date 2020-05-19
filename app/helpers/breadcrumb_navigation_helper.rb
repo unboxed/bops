f69232f3547d843902be67ffc6b998f8118984be
Evangelos Giataganas <e.giataganas@gmail.com>
2020-05-19 15:35:28 +0100

    Fix rubocop and move the create_breadcrumb_navigation to partial

--- app/helpers/breadcrumb_navigation_helper.rb
+++ app/helpers/breadcrumb_navigation_helper.rb
@@ -1,41 +1,35 @@
+# frozen_string_literal: true
+
 module BreadcrumbNavigationHelper
   PLANNING_APPLICATION_REGEX = /planning_applications\/[0-9]+\//
-
-  def ensure_navigation
-    @navigation ||= [ { title: "Home", path: root_path } ]
-  end
-
-  def ensure_planning_application_path(id)
-    { title: "Application", path: planning_application_path(id) }
-  end
 
   def navigation_add(title, path)
     if path.match?(PLANNING_APPLICATION_REGEX)
       ensure_navigation <<
           ensure_planning_application_path(params[:planning_application_id]) <<
-          { title: title, path: path}
+          { title: title, path: path }
     else
-      ensure_navigation << {title: "Application", path: nil}
+      ensure_navigation << { title: "Application", path: nil }
     end
   end
 
-  def concatenate_nav_elements(nav)
-    nav.each do |page|
-      if page == ensure_navigation.last
-        concat content_tag(:a, page[:title])
-      else
-        concat content_tag(:a, page[:title], href: page[:path])
-        concat " > "
-      end
-    end
+  def render_navigation
+    render partial: "breadcrumb_navigation", locals: { nav: ensure_navigation }
   end
 
-  def create_breadcrumbs
-    breadcrumbs = content_tag(:li, class: '"govuk-breadcrumbs__list-item"') do
-      unless ensure_navigation.length == 1
-        concatenate_nav_elements(ensure_navigation)
-      end
-    end
-    breadcrumbs
+  private
+
+  # rubocop:disable Rails/HelperInstanceVariable
+  def ensure_navigation
+    @navigation ||= home_navigation_path
+  end
+  # rubocop:enable Rails/HelperInstanceVariable
+
+  def home_navigation_path
+    [ { title: "Home", path: root_path } ]
+  end
+
+  def ensure_planning_application_path(id)
+    { title: "Application", path: planning_application_path(id) }
   end
 end
