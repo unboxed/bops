a1b72dd20c40f1bea5ca0612a12b324df86d4f29
Evangelos Giataganas <e.giataganas@gmail.com>
2020-06-01 12:20:29 +0100

    Add more specs for decision notice

--- spec/system/planning_applications/assessment_spec.rb
+++ spec/system/planning_applications/assessment_spec.rb
@@ -78,6 +78,26 @@ RSpec.describe "Planning Application Assessment", type: :system do
 
       click_link "Confirm decision notice"
 
+      expect(page).to have_content("Submit Recommendation")
+      expect(page).to have_content("Based on your answers given with the Permitted Development Policy Requirements, Permited Development should be #{planning_application.reload.assessor_decision.status}")
+
+      expect(planning_application.reload.assessor_decision.comment_met.present?).to be(true)
+      expect(page).to have_content("Your comments have been added to the decision notice.")
+
+      expect(page).to have_content("Certificate of lawfulness of proposed use or development: #{planning_application.reload.assessor_decision.status}")
+
+      # Applicant
+      expect(page).to have_content("#{planning_application.applicant.full_name}]")
+      # Date of Issue of this decision, TODO: implement to hold the decided_at
+      expect(page).to have_content("TBD")
+      # Application received
+      expect(page).to have_content("[#{planning_application.created_at.strftime("%d/%m/%Y")}]")
+      # Address, TODO: add a fixture test for this
+      # Application number
+      expect(page).to have_content("[#{planning_application.reference}]")
+
+      expect(page).to have_content("Certificate of lawful development (proposed) for the construction of [#{planning_application.description}]")
+
       click_button "Submit to manager"
 
       within(:assessment_step, "Confirm decision notice") do
