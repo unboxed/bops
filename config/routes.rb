7156bb6d85617b97f4d7ef2a7ba057433a9be657
Evangelos Giataganas <e.giataganas@gmail.com>
2020-06-05 13:29:08 +0100

    Use ruby’s % notation for arrays on routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
     get "/users/sign_out", to: "devise/sessions#destroy"
   end
 
-  resources :planning_applications, only: [:show, :index, :edit, :update] do
-    resources :decisions, only: [:new, :create, :edit, :update]
+  resources :planning_applications, only: %i[show index edit update] do
+    resources :decisions, only: %i[new create edit update]
   end
 
   get :healthcheck, to: proc { [200, {}, %w[OK]] }
