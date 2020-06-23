69d87d086a7bd125644ac49efb1f22a1f562f8d3
Rhian Lewis <rhianatwork@gmail.com>
2020-06-23 10:18:47 +0100

    added archived_at column to drawings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_000001) do
+ActiveRecord::Schema.define(version: 2020_06_23_091721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_000001) do
     t.bigint "planning_application_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "archived_at"
     t.index ["planning_application_id"], name: "index_drawings_on_planning_application_id"
   end
 
