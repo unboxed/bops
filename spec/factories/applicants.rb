c8a6aaea04764bbdd18c60dbaae8bd173f6e5fc8
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-06-23 17:08:05 +0100

    Use non-unique faker data for names
    For some reason this was suddenly causing issues with Faker attempting
    to generate unique values too many times. But I don't think these need
    to be unique.

--- spec/factories/applicants.rb
+++ spec/factories/applicants.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :applicant do
     agent
-    first_name { Faker::Name.unique.first_name }
-    last_name { Faker::Name.unique.last_name }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     phone { Faker::Base.numerify("+44 7### ######") }
     email { Faker::Internet.email }
     postcode { Faker::Address.postcode }
