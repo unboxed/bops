edb454aff6632e3502ae4bfa72641126f5db5689
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-07-10 11:03:13 +0100

    Allow more nested breadcrumbs
    Previously, we could only handle a page with two parents.

--- app/helpers/breadcrumb_navigation_helper.rb
+++ app/helpers/breadcrumb_navigation_helper.rb
@@ -1,37 +1,19 @@
 # frozen_string_literal: true
 
 module BreadcrumbNavigationHelper
-  PLANNING_APPLICATION_REGEX = /planning_applications\/[0-9]+\//
-
   def navigation_add(title, path)
-    if path.match?(PLANNING_APPLICATION_REGEX)
-      id = params[:planning_application_id] || params[:id]
-
-      ensure_navigation <<
-          ensure_planning_application_path(id) <<
-          { title: title, path: path }
-    else
-      ensure_navigation << { title: "Application", path: nil }
-    end
+    navigation << { title: title, path: path }
   end
 
   def render_navigation
-    render partial: "breadcrumb_navigation", locals: { nav: ensure_navigation }
+    render partial: "breadcrumb_navigation", locals: { nav: navigation }
   end
 
   private
 
   # rubocop:disable Rails/HelperInstanceVariable
-  def ensure_navigation
-    @navigation ||= home_navigation_path
+  def navigation
+    @_navigation ||= []
   end
   # rubocop:enable Rails/HelperInstanceVariable
-
-  def home_navigation_path
-    [ { title: "Home", path: root_path } ]
-  end
-
-  def ensure_planning_application_path(id)
-    { title: "Application", path: planning_application_path(id) }
-  end
 end
