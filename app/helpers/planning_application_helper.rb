3cf7d7e1ed6bfca5f503f87f8ce8b501fd4ec749
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-07-24 11:39:06 +0100

    Extract a few view helper methods
    Some of the task names vary depending on whether this is the "first
    pass" assessment or whether it's gone through a correction cycle. This
    commit adds some helpers to decide the right names.

--- app/helpers/planning_application_helper.rb
+++ app/helpers/planning_application_helper.rb
@@ -26,4 +26,28 @@ module PlanningApplicationHelper
       true if application.reviewer_decision&.valid?
     end
   end
+
+  def assess_proposal_step_name(planning_application)
+    if planning_application.correction?
+      step_name = "Reassess the proposal"
+    else
+      step_name = "Assess the proposal"
+    end
+  end
+
+  def submit_proposal_step_name(planning_application)
+    if planning_application.correction?
+      step_name = "Resubmit the recommendation"
+    else
+      step_name = "Submit the recommendation"
+    end
+  end
+
+  def review_proposal_step_name(planning_application)
+    if planning_application.correction?
+      step_name = "Review the corrections"
+    else
+      step_name = "Review the recommendation"
+    end
+  end
 end
