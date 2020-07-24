31fc1ada955f556b5f471fc345058354fdc3e4ec
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-07-24 11:07:43 +0100

    Toggle correction div display
    This commit ensures that the correct div is hidden when the manager
    chooses to agree with the assessor.

--- app/javascript/controllers/correction_controller.js
+++ app/javascript/controllers/correction_controller.js
@@ -3,11 +3,11 @@ import {Controller} from "stimulus"
 export default class extends Controller {
     static targets = ["textArea", "correctionDiv"]
 
-    addCorrection(event) {
-        this.correctionDivTarget.style.display = "block";
-        if (event.target.id === "status-granted-conditional") {
-            this.textAreaTarget.id = "correction";
-            this.textAreaTarget.name = "decision[correction]";
+    toggleCorrection(event) {
+        if (event.target.id === "reviewer-disagrees-conditional") {
+            this.correctionDivTarget.style.display = "block";
+        } else {
+            this.correctionDivTarget.style.display = "none";
         }
     }
 }
