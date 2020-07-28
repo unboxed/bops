eb41fca618e72948cf8546ef32a03b82ce3a2c2b
Evangelos Giataganas <e.giataganas@gmail.com>
2020-07-28 17:33:57 +0100

    Add validation on public_comment

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -9,7 +9,18 @@ class Decision < ApplicationRecord
   validates :status, inclusion: { in: ["granted", "refused"],
     message: "Please select Yes or No" }
 
+  validate :validate_public_comment
+
   def refused_with_public_comment?
     refused? && public_comment.present?
+  end
+
+  def validate_public_comment
+    if refused? && user.assessor? && public_comment.blank?
+      errors.add(
+        :public_comment,
+        "Please provide which GDPO policy (or policies) have not been met."
+      )
+    end
   end
 end
--- spec/system/planning_applications/assessing_spec.rb
+++ spec/system/planning_applications/assessing_spec.rb
@@ -153,5 +153,27 @@ RSpec.describe "Planning Application Assessment", type: :system do
     end
   end
 
+  scenario "shows the public_comment error message" do
+    within("#in_assessment") do
+      click_link planning_application.reference
+    end
+
+    expect(page).not_to have_link("Submit the recommendation")
+
+    click_link "Assess the proposal"
+
+    choose "No"
+
+    click_button "Save"
+
+    within(".govuk-error-message") do
+      expect(page).to have_content("Please provide which GDPO policy (or policies) have not been met.")
+    end
+
+    click_link "Home"
+
+    expect(page).not_to have_css(".app-task-list__task-completed")
+  end
+
   include_examples "assessor decision error message"
 end
