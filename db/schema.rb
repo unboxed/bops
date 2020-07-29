7f2bf7300a955faef6b3c1147dcaec039536b568
Evangelos Giataganas <e.giataganas@gmail.com>
2020-07-29 10:15:44 +0100

    Add private_comment on decision table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_151340) do
+ActiveRecord::Schema.define(version: 2020_07_29_091424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2020_07_28_151340) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "status"
     t.text "public_comment"
+    t.text "private_comment"
     t.index ["planning_application_id"], name: "index_decisions_on_planning_application_id"
     t.index ["user_id"], name: "index_decisions_on_user_id"
   end
