85de097eea4dd876c5daafa22e17566068e90692
Evangelos Giataganas <e.giataganas@gmail.com>
2020-08-05 14:58:44 +0100

    Extract the completed_tag logic into a helper method

--- app/helpers/planning_application_helper.rb
+++ app/helpers/planning_application_helper.rb
@@ -22,4 +22,30 @@ module PlanningApplicationHelper
       "View assessed applications"
     end
   end
+
+  def proposal_step_mark_completed?(step_name, application)
+    case step_name
+    when "Assess the proposal"
+      application.assessor_decision&.valid?
+    when "Review the recommendation"
+      application.reviewer_decision&.valid?
+    when "View the assessment"
+      application.determined?
+    else
+      false
+    end
+  end
+
+  def recommendation_step_mark_completed?(step_name, application)
+    case step_name
+    when "Submit the recommendation"
+      application.awaiting_determination?
+    when "Publish the recommendation"
+      application.determined?
+    when "View the decision notice"
+      application.determined?
+    else
+      false
+    end
+  end
 end
