f5eeeaf0c7532c1feae79282147775f49edbcd6e
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-08-18 12:30:38 +0100

    WIP re-routing drawing management things

--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,37 @@ Rails.application.routes.draw do
     resources :decisions, only: %i[new create edit update show]
 
     resources :drawings, only: %i[index new create] do
+      # Numbering
+      # /planning_applications/1/drawings/edit_numbers
+
+      # /planning_applications/1/drawings/2/numbers/edit  ?
+
+      # /planning_applications/1/edit_drawing_numbers  ?
       get :edit_numbers, on: :collection
       put :update_numbers, on: :collection
 
+      # Archiving
+
+      # /planning_applications/1/drawings/1/archive
+      # /planning_applications/1/drawings/1/confirm
+      # /planning_applications/1/drawings/1/validate_step
+
+      # /planning_applications/1/drawings/1/archive/new
+      # /planning_applications/1/drawings/1/archive/new/confirm
+      # /planning_applications/1/drawings/1/archive/new/validate_step
+
       get :archive
       get :confirm
-
-      post :confirm_new, on: :collection
-
       post :validate_step
+
+      # Upload - should this be on collection or something else?
+
+      # planning_applications/1/drawing_upload/new
+      # planning_applications/1/drawing_upload/confirm_new
+      # planning_applications/1/drawing_upload/create
+
+      post :confirm, on: :new
+      # /planning_applications/1/drawings/1  ?
     end
   end
 
