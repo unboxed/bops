860568b4da432ec1c331644ba073bec357b80d1a
melzreal <melissa.real@hotmail.com>
2020-11-25 15:43:35 +0000

    Prevent login from different subdomain
    - Prevent users that belong to subdomain southwark from logging in to lambeth subdomain and vice versa
    - This is achieved by using the routes file to pass the subdomain to the params used by the devise authentication method "find_for_authentication"
    - Note we kept validatable, meaning emails must be unique regardless of subdomain. If we want to allow for the same email to have accounts in different subdomains, we will need to remove / customise the devise validatable implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,14 @@ class User < ApplicationRecord
   enum role: { assessor: 0, reviewer: 1, admin: 2 }
 
   devise :database_authenticatable, :recoverable,
-         :rememberable, :validatable
+         :rememberable, :validatable, request_keys: [:subdomains]
 
   has_many :decisions, dependent: :restrict_with_exception
   has_many :planning_applications, through: :decisions
   belongs_to :local_authority
+
+  def self.find_first_by_auth_conditions(conditions)
+    local_authority = LocalAuthority.find_by(subdomain: conditions[:subdomains].first)
+    find_by(email: conditions[:email], local_authority: local_authority.id)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,4 +47,24 @@ RSpec.describe User, type: :model do
     email = build(:user, email: nil)
     expect(email).to_not be_valid
   end
+
+  it "does not allow for duplicate users within the same domain" do
+    domain = create(:local_authority)
+    user_one = create(:user, email: "pompom@pom.com", local_authority: domain)
+    user_two = build(:user, email: "pompom@pom.com", local_authority: domain)
+
+    expect(user_two.save).to be false
+    expect(user_two.errors.messages[:email]).to include("has already been taken")
+  end
+
+  it "does not allow for duplicate users in separate domains" do
+    domain_one = create(:local_authority)
+    domain_two = create(:local_authority)
+
+    user_one = create(:user, email: "gali@galileo.com", local_authority: domain_one)
+    user_two = build(:user, email: "gali@galileo.com", local_authority: domain_two)
+
+    expect(user_two.save).to be false
+    expect(user_two.errors.messages[:email]).to include("has already been taken")
+  end
 end
