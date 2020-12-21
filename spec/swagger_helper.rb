d0fb0a53d6ad3d22419e6e051481abc7a1f15485
James Darling <james@abscond.org>
2020-12-21 12:20:54 +0000

    Switch to YAML for swagger docs
    Yaml is a lot easier to read for this syntax, and seems to be the preferred format for most swagger tooling.

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -4,8 +4,9 @@ require 'rails_helper'
 
 RSpec.configure do |config|
   config.swagger_root = Rails.root.join('swagger').to_s
+  config.swagger_format = :yaml
   config.swagger_docs = {
-      '/v1/swagger_doc.json' => {
+      '/v1/swagger_doc.yaml' => {
           openapi: '3.0.1',
           info: {
               title: 'Back-office Planning System',
