8f9ae7e6be1c5f374e12dcd0b65ba1e30a1624a5
James Darling <james@abscond.org>
2021-01-25 17:56:56 +0000

    Remove production default url options for mailer
    In 0879918dc3da9891c6b239a03897e7ea75c06ea1 I ensured that we passed the request host through to any mailers we sent, given that we are multi-tenant, and therefore we don't know the host from the environment.
    
    However, I missed that this default_url_options had been set in production.rb, so the host was being set correctly, but the port was being set as 3000 by default, which is wrong.
    
    I believe that removing it is the best solution: I would rather error than have an incorrect URL silently get sent.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,9 +95,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options =
-    { host: ENV["PRODUCTION_HOST"], port: 3000 }
-
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
