a9c738b450d07b8c12318149df136ce280a45217
James Darling <james@abscond.org>
2021-02-12 14:24:18 +0000

    Make reviewer comment always optional on review
    Previously the reviewer comment was required if rejected, and not available if aproved. Now it is optional for both.
    
    Co-Authored-By: Melissa  <melzreal@users.noreply.github.com>

--- spec/system/planning_applications/reviewing_spec.rb
+++ spec/system/planning_applications/reviewing_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe "Planning Application Reviewing", type: :system do
     end
 
     choose "Yes"
+    fill_in "Review comment", with: "Reviewer private comment"
     click_button "Save"
     click_link "Publish"
     click_button "Determine application"
@@ -39,6 +40,7 @@ RSpec.describe "Planning Application Reviewing", type: :system do
     expect(planning_application.status).to eq("determined")
     expect(planning_application.recommendations.last.reviewer).to eq(reviewer)
     expect(planning_application.recommendations.last.reviewed_at).not_to be_nil
+    expect(planning_application.recommendations.last.reviewer_comment).to eq("Reviewer private comment")
     expect(ActionMailer::Base.deliveries.count).to eq(delivered_emails + 1)
   end
 
@@ -46,7 +48,7 @@ RSpec.describe "Planning Application Reviewing", type: :system do
     delivered_emails = ActionMailer::Base.deliveries.count
     click_link "Review Assessment"
     choose "No"
-    fill_in "Please provide comments on why you don't agree.", with: "Reviewer private comment"
+    fill_in "Review comment", with: "Reviewer private comment"
     click_button "Save"
     expect(page).not_to have_link("Publish")
 
@@ -69,15 +71,13 @@ RSpec.describe "Planning Application Reviewing", type: :system do
       expect(page).not_to have_content("Reviewer private comment")
     end
 
-    expect(page).to have_field("Please provide comments on why you don't agree.", with: "Reviewer private comment")
+    expect(page).to have_field("Review comment", with: "Reviewer private comment")
 
     choose "No"
-    fill_in "Please provide comments on why you don't agree.", with: "Edited reviewer private comment"
+    fill_in "Review comment", with: "Edited reviewer private comment"
     click_button "Save"
 
     recommendation.reload
     expect(recommendation.reviewer_comment).to eq("Edited reviewer private comment")
   end
-
-  it "raises error if rejection doesn't include private comment"
 end
