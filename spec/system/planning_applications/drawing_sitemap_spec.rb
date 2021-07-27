d26a6d73a7aeedd3b1b4c0063d388d1e565a299a
James Darling <james@abscond.org>
2021-07-27 13:20:57 +0100

    Allow editing of sitemap by officer

--- spec/system/planning_applications/drawing_sitemap_spec.rb
+++ spec/system/planning_applications/drawing_sitemap_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Drawing a sitemap on a planning application", type: :system do
       create :planning_application, local_authority: @default_local_authority
     end
 
-    it "is possible to create a sitemap when application is not_started" do
+    it "is possible to create and edit a sitemap when application is not_started" do
       click_button "Site map"
       expect(page).to have_content("No digital sitemap provided")
       click_link "Draw digital sitemap"
@@ -26,6 +26,14 @@ RSpec.describe "Drawing a sitemap on a planning application", type: :system do
 
       expect(page).to have_content("Site boundary has been updated")
       expect(page).not_to have_content("No digital sitemap provided")
+
+      click_button "Site map"
+      click_link "Redraw digital sitemap"
+
+      execute_script 'document.getElementById("planning_application_boundary_geojson").setAttribute("value", \'{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[-0.054597,51.537331],[-0.054588,51.537287],[-0.054453,51.537313]]]}}\')'
+      click_button "Save"
+
+      expect(page).to have_content("Site boundary has been updated")
     end
   end
 end
