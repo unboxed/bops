1d08bc995655e84494291178134733a58bc5c36f
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-09-21 16:49:57 +0100

    policies: add specs/fixes to handle the planning application states

---
+++ spec/system/planning_applications/policy_assessment_spec.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Planning Application Assessment", type: :system do
+  let!(:assessor) { create :user, :assessor, local_authority: @default_local_authority }
+
+  before do
+    sign_in assessor
+  end
+
+  context "when the application hasn't been started" do
+    let!(:planning_application) do
+      create :planning_application, :not_started, local_authority: @default_local_authority
+    end
+
+    it "cannot be assessed against policies" do
+      visit planning_application_path(planning_application)
+
+      expect(page).not_to have_link "Add assessment area"
+    end
+  end
+
+  context "when the application is in assessment" do
+    let!(:planning_application) do
+      create :planning_application, :in_assessment, local_authority: @default_local_authority
+    end
+
+    before do
+      visit planning_application_path(planning_application)
+    end
+
+    def select_class(class_name)
+      visit planning_application_path(planning_application)
+
+      click_link "Add assessment area"
+
+      choose "Part 1"
+      click_button "Continue"
+
+      check "Class #{class_name}"
+
+      click_button "Add classes"
+    end
+
+    it "has a link to add an assessment area" do
+      expect(page).to have_link "Add assessment area"
+    end
+
+    it "allows adding classes to assess against" do
+      select_class "AA"
+      select_class "C"
+
+      expect(current_path).to eq planning_application_path(planning_application)
+
+      expect(page).to have_text "classes successfully added"
+
+      within("#assess-policy-section") do
+        expect(page).to have_link "Part 1, Class AA"
+        expect(page).to have_link "Part 1, Class C"
+      end
+    end
+
+    it "defaults the policies to be determined" do
+      select_class "AA"
+
+      within("#assess-policy-section") do
+        expect(page).to have_text "in assessment"
+      end
+
+      click_link "Part 1, Class AA"
+
+      find_all("input:checked") do |node|
+        expect(node.value).to eq "to_be_determined"
+      end
+    end
+
+    describe "when the application is past assessment" do
+      before do
+        select_class "AA"
+
+        planning_application.decision = "All good"
+        planning_application.assess
+        planning_application.determine
+
+        visit planning_application_path(planning_application)
+      end
+
+      it "doesn't let the assessor add further classes" do
+        within("#assess-policy-section") do
+          expect(page).not_to have_text "Add assessment area"
+        end
+      end
+
+      it "doesn't allow changing the policies status" do
+        click_link "Part 1, Class AA"
+
+        find_all("input[type='radio']") do |node|
+          expect(node).to be_disabled
+        end
+      end
+    end
+  end
+end
