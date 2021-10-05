d6b5e49e3677fe8f517fa6830cb564b05ef0e4d0
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-10-05 16:34:22 +0100

    cucumber: introduce Cucumber suite

---
+++ features/support/capybara.rb
@@ -0,0 +1,20 @@
+Capybara.server = :puma, { Silent: true }
+
+Capybara.register_driver :chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[window-size=1280,960] },
+  )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
+Capybara.register_driver :chrome_headless do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless no-sandbox window-size=1280,960] },
+  )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+end
+
+Capybara.javascript_driver = ENV.fetch("JS_DRIVER", "chrome_headless").to_sym
+Capybara.automatic_label_click = true
