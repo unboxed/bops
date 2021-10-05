860d64a2e6b34f0067508c49ecb0e4beb29f3dae
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-10-05 15:46:02 +0100

    validation requests: restore some bits lost to rebase

--- app/helpers/planning_application_helper.rb
+++ app/helpers/planning_application_helper.rb
@@ -156,8 +156,8 @@ module PlanningApplicationHelper
 
   def validation_request_summary(validation_requests, planning_application)
     if planning_application.invalidated?
-      "This application has #{pluralize(validation_requests.select { |req| req.state == 'open' }.count, 'unresolved validation request')} and #{pluralize(validation_requests.select { |req| req.state == 'closed' }.count, 'resolved validation request')}"
-    elsif planning_application.validation_requests.blank? && (planning_application.recommendable? || planning_application.closed?)
+      "This application has #{pluralize(validation_requests.count(&:open?), 'unresolved validation request')} and #{pluralize(validation_requests.count(&:closed?), 'resolved validation request')}"
+    elsif planning_application.validation_requests.none? && (planning_application.recommendable? || planning_application.closed?)
       "This application had no validation requests"
     elsif planning_application.recommendable? || planning_application.closed? && planning_application.validation_requests.present?
       "This application has #{pluralize(validation_requests.count(&:closed?), 'resolved validation request')}"
