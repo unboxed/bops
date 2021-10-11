228e2c8fe93f7f1a5b1cc72a4f1d9b5a2fdf678f
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-10-11 11:03:36 +0100

    local authority: ensure the Faker subdomain is unique

--- spec/factories/local_authorities.rb
+++ spec/factories/local_authorities.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :local_authority do
     name { Faker::Name.name }
-    subdomain { Faker::Internet.domain_word }
+    subdomain { Faker::Internet.unique.domain_word }
     signatory_name { Faker::FunnyName.two_word_name }
     signatory_job_title { "Director" }
     enquiries_paragraph { Faker::Lorem.unique.sentence }
