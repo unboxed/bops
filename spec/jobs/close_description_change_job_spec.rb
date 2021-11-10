7f269e3300dac68295848edc2a9ee222c49bfee3
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-11-10 14:41:54 +0000

    spec: make the auto-closing job spec more explicit

--- spec/jobs/close_description_change_job_spec.rb
+++ spec/jobs/close_description_change_job_spec.rb
@@ -3,22 +3,21 @@
 require "rails_helper"
 
 RSpec.describe CloseDescriptionChangeJob, type: :job do
-  let!(:planning_application) do
-    create :planning_application
-  end
+  let!(:planning_application) { create :planning_application }
+
   let!(:description_change_request) do
     create :description_change_validation_request,
            planning_application: planning_application,
            created_at: 6.business_days.ago
   end
 
-  it "updates all required statuses" do
-    expect do
-      described_class.perform_now
-      planning_application.reload
-      description_change_request.reload
-    end.to change(planning_application, :description)
-      .and change(description_change_request, :state)
-      .and change(description_change_request, :auto_closed)
+  it "changes the application's description" do
+    expect { described_class.perform_now }.to(change { planning_application.reload.description })
+  end
+
+  it "auto-closes the change request" do
+    expect { described_class.perform_now }
+      .to(change { description_change_request.reload.state }.from("open").to("closed")
+          .and(change { description_change_request.reload.auto_closed }.from(false).to(true)))
   end
 end
