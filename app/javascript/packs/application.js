7c41e6cbb7f48bf56bf61c9f95a48fd1fd9d16d6
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-11-24 12:43:37 +0000

    js: remove some unused JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "stylesheets"
+import "stylesheets";
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
 
-import '@opensystemslab/map';
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
-import "controllers"
+import "@opensystemslab/map";
