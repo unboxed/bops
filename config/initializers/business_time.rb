2ecd4e2e24bb1954ee8c8acb1f76d1235284f48a
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-11-26 17:19:30 +0000

    config: add some extensions to time
    It's just a bit of syntactic sugar but it's nice.

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -6,3 +6,17 @@ BusinessTime::Config.load(Rails.root.join("config/business_time.yml").to_s)
 #  BusinessTime::Config.beginning_of_workday = "10:00 am"
 #  BusinessTime::Config.end_of_workday = "11:30 am"
 #  BusinessTime::Config.holidays << Date.parse("August 4th, 2010")
+
+module Bops
+  module TimeExtensions
+    module ClassMethods
+      def next_immediate_business_day(time)
+        Time.roll_forward(time).to_date
+      end
+    end
+  end
+end
+
+class Time
+  extend Bops::TimeExtensions::ClassMethods
+end
