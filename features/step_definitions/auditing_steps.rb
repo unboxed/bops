164e95528497882f9fae7d8efc3d010ca9164465
melzreal <melissa.real@hotmail.com>
2021-12-17 10:51:41 +0000

    Add cucumber test for accordion text display

--- features/step_definitions/auditing_steps.rb
+++ features/step_definitions/auditing_steps.rb
@@ -16,6 +16,23 @@ Then("there is an audit entry with") do |content|
   end
 end
 
+Then("there is an audit log entry in the index accordion with {string}") do |content|
+  steps %(
+    When I view the planning application
+    And I press "Audit log"
+    Then the page contains "#{content}"
+  )
+end
+
+Then("there is an audit log entry in the validate form accordion with {string}") do |content|
+  steps %(
+    When I view the planning application
+    And I press "Validate application"
+    And I press "Audit log"
+    Then the page contains "#{content}"
+  )
+end
+
 When("I visit the audit log") do
   steps %(
     When I view the planning application
