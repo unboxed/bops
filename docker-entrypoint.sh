381670f0c27c69a50e6a548227bbc406db264846
Evangelos Giataganas <e.giataganas@gmail.com>
2022-01-11 15:24:21 +0200

    Rework and use docker-entrypoint
    Sometimes we were getting an error that a server was already
    running. So we had to go and manually delete the server.pid
    file. We now use docker-entrypoint script to delete server.pid.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,12 @@
 
 set -e
 
-echo 'Waiting for a connection with postgres...'
-
-until psql -h "postgres" -U "postgres" -c '\q' > /dev/null 2>&1; do
-  sleep 1
-done
-
-echo "Connected to postgres..."
-
 echo "Bundling gems"
 bundle check || bundle install
 
-if [ "$2" == 'server' ]; then
-  if [ -f /app/tmp/pids/server.pid ]; then
-    echo 'Deleting old server pid file ...'
-    rm -f /app/tmp/pids/server.pid
-  fi
+if [ -f /app/tmp/pids/server.pid ]; then
+  echo 'Deleting old server pid file ...'
+  rm -f /app/tmp/pids/server.pid
 fi
 
 # parameters will pass to bundle exec from docker-compose
