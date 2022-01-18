845f6470ab05210a4277873b61e6f794b0b49cd4
Ben <benbaumann95@hotmail.co.uk>
2022-01-18 12:36:03 +0000

    Extract planning application AASM status into concern - This logic can live in a separate module and be included in the model PlanningApplication. This should help navigation and declutter our model a bit

---
+++ app/models/concerns/planning_application_status.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require "aasm"
+
+module PlanningApplicationStatus
+  extend ActiveSupport::Concern
+
+  IN_PROGRESS_STATUSES = %i[not_started in_assessment invalidated awaiting_determination awaiting_correction].freeze
+
+  included do
+    include AASM
+
+    scope :not_started_and_invalid, lambda {
+      where("status = 'not_started' OR status = 'invalidated'")
+    }
+    scope :under_assessment, lambda {
+      where("status = 'in_assessment' OR status = 'assessment_in_progress' OR status = 'awaiting_correction'")
+    }
+    scope :closed, lambda {
+      where("status = 'determined' OR status = 'withdrawn' OR status = 'returned' OR status = 'closed'")
+    }
+
+    aasm.attribute_name :status
+
+    aasm no_direct_assignment: true do
+      state :not_started, initial: true
+      state :invalidated, display: "invalid"
+      state :assessment_in_progress
+      state :in_assessment
+      state :awaiting_determination
+      state :awaiting_correction
+      state :determined
+      state :returned
+      state :withdrawn
+      state :closed
+
+      event :start do
+        transitions from: %i[not_started invalidated in_assessment], to: :in_assessment, guard: :has_validation_date?
+      end
+
+      event :save_assessment do
+        transitions from: %i[in_assessment assessment_in_progress], to: :assessment_in_progress
+
+        after do
+          save(validate: false)
+        end
+      end
+
+      event :assess do
+        transitions from: %i[in_assessment assessment_in_progress awaiting_correction], to: :awaiting_determination,
+                    guard: :decision_present?
+      end
+
+      event :invalidate do
+        transitions from: :not_started, to: :invalidated, guard: :pending_validation_requests? do
+          after { pending_validation_requests.each(&:mark_as_sent!) }
+        end
+      end
+
+      event :determine do
+        transitions from: :awaiting_determination, to: :determined
+      end
+
+      event :request_correction do
+        transitions from: :awaiting_determination, to: :awaiting_correction
+      end
+
+      event :return do
+        transitions from: IN_PROGRESS_STATUSES, to: :returned,
+                    after: proc { |comment| update!(closed_or_cancellation_comment: comment) }
+      end
+
+      event :withdraw do
+        transitions from: IN_PROGRESS_STATUSES, to: :withdrawn,
+                    after: proc { |comment| update!(closed_or_cancellation_comment: comment) }
+      end
+
+      event :close do
+        transitions from: IN_PROGRESS_STATUSES, to: :closed,
+                    after: proc { |comment| update!(closed_or_cancellation_comment: comment) }
+      end
+
+      after_all_transitions :timestamp_status_change # FIXME: https://github.com/aasm/aasm#timestamps
+    end
+  end
+end
