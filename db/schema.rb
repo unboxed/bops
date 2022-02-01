8b61868f4f8f5163c925ed011ee5941a5c045f99
Evangelos Giataganas <e.giataganas@gmail.com>
2022-02-01 14:04:12 +0200

    Add missing foreign key between audit and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_27_164323) do
+ActiveRecord::Schema.define(version: 2022_02_01_115046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -315,6 +315,7 @@ ActiveRecord::Schema.define(version: 2022_01_27_164323) do
   add_foreign_key "additional_document_validation_requests", "users"
   add_foreign_key "audits", "api_users"
   add_foreign_key "audits", "planning_applications"
+  add_foreign_key "audits", "users"
   add_foreign_key "description_change_validation_requests", "planning_applications"
   add_foreign_key "description_change_validation_requests", "users"
   add_foreign_key "documents", "additional_document_validation_requests"
