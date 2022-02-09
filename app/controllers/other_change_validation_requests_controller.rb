c9e5c4b0dc8611dbace443810e048f28fa6a224a
Evangelos Giataganas <e.giataganas@gmail.com>
2022-02-09 16:01:46 +0200

    Audit other_change_validation_request on after_create callback

--- app/controllers/other_change_validation_requests_controller.rb
+++ app/controllers/other_change_validation_requests_controller.rb
@@ -19,13 +19,6 @@ class OtherChangeValidationRequestsController < ValidationRequestsController
       email_and_timestamp(@other_change_validation_request) if @planning_application.invalidated?
 
       flash[:notice] = "Other validation change request successfully created."
-      if @planning_application.invalidated?
-        audit("other_change_validation_request_sent", audit_item(@other_change_validation_request),
-              @other_change_validation_request.sequence)
-      else
-        audit("other_change_validation_request_added", audit_item(@other_change_validation_request),
-              @other_change_validation_request.sequence)
-      end
       redirect_to planning_application_validation_requests_path(@planning_application)
     else
       render :new
@@ -46,10 +39,6 @@ class OtherChangeValidationRequestsController < ValidationRequestsController
 
   def other_change_validation_request_params
     params.require(:other_change_validation_request).permit(:summary, :suggestion)
-  end
-
-  def audit_item(other_change_validation_request)
-    { summary: other_change_validation_request.summary, suggestion: other_change_validation_request.suggestion }.to_json
   end
 
   def set_other_validation_request
--- app/models/other_change_validation_request.rb
+++ app/models/other_change_validation_request.rb
@@ -10,6 +10,7 @@ class OtherChangeValidationRequest < ApplicationRecord
   validates :suggestion, presence: true
 
   validate :response_is_present?
+  after_create :create_audit!
 
   def response_is_present?
     errors.add(:base, "some suggestion error here") if closed? && response.blank?
@@ -19,5 +20,23 @@ class OtherChangeValidationRequest < ApplicationRecord
 
   def audit_api_comment
     { response: response }.to_json
+  end
+
+  def create_audit!
+    event = planning_application.invalidated? ? "sent" : "added"
+    create_audit_for!(event)
+  end
+
+  def create_audit_for!(event)
+    audit_created!(
+      activity_type: "other_change_validation_request_#{event}",
+      activity_information: sequence.to_s,
+      audit_comment: audit_comment
+    )
+  end
+
+  def audit_comment
+    { summary: summary,
+      suggestion: suggestion }.to_json
   end
 end
