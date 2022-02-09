85227b2446b6291f7d2ad1ba4e70e230d71276ff
Evangelos Giataganas <e.giataganas@gmail.com>
2022-02-09 13:07:25 +0200

    Audit other_change_validation_request for the api

--- app/models/other_change_validation_request.rb
+++ app/models/other_change_validation_request.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class OtherChangeValidationRequest < ApplicationRecord
+  include AuditableModel
+
   include ValidationRequest
 
   belongs_to :planning_application
   belongs_to :user
+
+  delegate :audits, to: :planning_application
 
   validates :summary, presence: true
   validates :suggestion, presence: true
@@ -13,5 +17,19 @@ class OtherChangeValidationRequest < ApplicationRecord
 
   def response_is_present?
     errors.add(:base, "some suggestion error here") if closed? && response.blank?
+  end
+
+  def create_api_audit!
+    audit_created!(
+      activity_type: "other_change_validation_request_received",
+      activity_information: sequence.to_s,
+      audit_comment: audit_api_comment
+    )
+  end
+
+  private
+
+  def audit_api_comment
+    { response: response }.to_json
   end
 end
