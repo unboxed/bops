01d5cd717908c5d895a619e18ee69a5c7eddef71
Evangelos Giataganas <e.giataganas@gmail.com>
2022-03-29 19:37:53 +0300

    Improve sitemap update action

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -28,22 +28,24 @@ class SitemapsController < AuthenticationController
   end
 
   def update
-    new_map = @planning_application.boundary_geojson.blank?
-    @planning_application.boundary_geojson = params[:planning_application][:boundary_geojson]
-    @planning_application.boundary_created_by = current_user
-    @planning_application.save!
-
-    if new_map
-      @planning_application.audit_boundary_geojson!("created")
-    else
-      @planning_application.audit_boundary_geojson!("updated")
+    respond_to do |format|
+      if @planning_application.update(boundary_geojson: boundary_geojson_params[:boundary_geojson],
+                                      boundary_created_by: current_user)
+        format.html do
+          redirect_to planning_application_validation_tasks_path(@planning_application),
+                      notice: "Site boundary has been updated"
+        end
+      else
+        format.html { render :edit }
+      end
     end
-
-    redirect_to planning_application_validation_tasks_path(@planning_application),
-                notice: "Site boundary has been updated"
   end
 
   private
+
+  def boundary_geojson_params
+    params.require(:planning_application).permit(:boundary_geojson)
+  end
 
   def red_line_boundary_params
     if params[:planning_application]
