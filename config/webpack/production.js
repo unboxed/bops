10f79c5c24ef784d1f1f9ab741a93f0b239f8aac
Evangelos Giataganas <e.giataganas@gmail.com>
2022-05-13 13:34:16 +0300

    Disable JavaScript source maps in production

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+var config = environment.toWebpackConfig()
+config.devtool = 'nosources-source-map'
+
+module.exports = config
