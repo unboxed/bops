4ee86d94d809dc9e05f21ab0754290f2f1060d9f
allbecauseyoutoldmeso <allbecauseyoutoldmeso@yahoo.co.uk>
2022-08-03 13:39:36 +0100

    Add option to search applications by description

--- app/services/planning_application_search.rb
+++ app/services/planning_application_search.rb
@@ -8,12 +8,46 @@ class PlanningApplicationSearch
   validates :query, presence: true
 
   def results
-    if valid?
-      planning_applications.where(
-        "LOWER(reference) LIKE ?", "%#{query.downcase}%"
-      )
-    else
-      planning_applications
-    end
+    valid? ? records_matching_query : planning_applications
+  end
+
+  private
+
+  def records_matching_query
+    records_matching_reference.presence || records_matching_description
+  end
+
+  def records_matching_reference
+    planning_applications.where(
+      "LOWER(reference) LIKE ?",
+      "%#{query.downcase}%"
+    )
+  end
+
+  def records_matching_description
+    planning_applications
+      .select(sanitized_select_sql)
+      .where(where_sql, query_terms)
+      .order(rank: :desc)
+  end
+
+  def sanitized_select_sql
+    ActiveRecord::Base.sanitize_sql_array([select_sql, query_terms])
+  end
+
+  def select_sql
+    "*,
+    ts_rank(
+      to_tsvector('english', description),
+      to_tsquery('english', ?)
+    ) AS rank"
+  end
+
+  def where_sql
+    "to_tsvector('english', description) @@ to_tsquery('english', ?)"
+  end
+
+  def query_terms
+    @query_terms ||= query.split.join(" | ")
   end
 end
