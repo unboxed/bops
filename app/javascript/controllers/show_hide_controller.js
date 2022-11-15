58b2094785ccb0875379a40c245009111d2ee9f6
allbecauseyoutoldmeso <allbecauseyoutoldmeso@yahoo.co.uk>
2022-11-15 16:27:52 +0000

    Allow reviewer to edit assessor comments on policies

--- app/javascript/controllers/show_hide_controller.js
+++ app/javascript/controllers/show_hide_controller.js
@@ -1,21 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["hideable"]
+  static targets = ["toggleable"]
 
-  handleChange(event) {
-    event.target.checked ? this.hide() : this.show()
-  }
-
-  hide() {
-    this.hideableTargets.forEach(element => {
-      element.classList.add("display-none")
-    })
-  }
-
-  show() {
-    this.hideableTargets.forEach(element => {
-      element.classList.remove("display-none")
+  handleEvent(_event) {
+    this.toggleableTargets.forEach(element => {
+      element.classList.toggle("display-none")
     })
   }
 }
