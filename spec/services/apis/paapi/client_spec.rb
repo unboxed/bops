98daa8e81365f624f7b26173abb76e3bdf56b957
allbecauseyoutoldmeso <allbecauseyoutoldmeso@yahoo.co.uk>
2022-12-19 12:36:18 +0000

    Fix RSpec/NoExpectionExample errors

--- spec/services/apis/paapi/client_spec.rb
+++ spec/services/apis/paapi/client_spec.rb
@@ -6,13 +6,8 @@ RSpec.describe Apis::Paapi::Client do
   let(:client) { described_class.new }
 
   describe "#call" do
-    let(:faraday_connection) { spy }
-    let(:url) { "https://staging.paapi.services/api/v1" }
-
-    it "makes a Faraday connection" do
-      allow(Faraday).to receive(:new).with(url: url).and_yield(faraday_connection)
-
-      client.call("100081043511")
+    it "is successful" do
+      expect(client.call("100081043511").status).to eq(200)
     end
   end
 end
