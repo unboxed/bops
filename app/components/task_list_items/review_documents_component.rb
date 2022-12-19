4319929ead22cd38e28458e13235c70cd011035f
allbecauseyoutoldmeso <allbecauseyoutoldmeso@yahoo.co.uk>
2022-12-19 13:03:26 +0000

    Move task list item for reviewing documents to components

---
+++ app/components/task_list_items/review_documents_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module TaskListItems
+  class ReviewDocumentsComponent < TaskListItems::BaseComponent
+    def initialize(planning_application:)
+      @planning_application = planning_application
+    end
+
+    attr_reader :planning_application
+
+    def link_text
+      t(".review_documents_for")
+    end
+
+    def link_path
+      planning_application_review_documents_path(planning_application)
+    end
+
+    def status_tag_component
+      StatusTags::BaseComponent.new(
+        status: planning_application.review_documents_for_recommendation_status.to_sym
+      )
+    end
+  end
+end
