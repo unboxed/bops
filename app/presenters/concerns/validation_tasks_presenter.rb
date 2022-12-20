42a5078c9a39f2873890f5ca78f67e134d478237
allbecauseyoutoldmeso <allbecauseyoutoldmeso@yahoo.co.uk>
2022-12-20 15:55:09 +0000

    Move other validation request task list item to components

--- app/presenters/concerns/validation_tasks_presenter.rb
+++ app/presenters/concerns/validation_tasks_presenter.rb
@@ -3,38 +3,9 @@
 module ValidationTasksPresenter
   extend ActiveSupport::Concern
 
-  STATUSES = ["Not checked yet", "Not started", "Invalid", "Valid", "Updated"].freeze
-
-  STATUS_COLOURS = {
-    "Not checked yet": "grey",
-    "Not started": "grey",
-    Invalid: "red",
-    Valid: "green",
-    Updated: "yellow"
-  }.freeze
-
   included do
     def items_counter
       ValidationTasks::ItemsCounterPresenter.new(@template, @planning_application).items_count
-    end
-
-    def other_change_task_list(other_change_validation_request)
-      ValidationTasks::OtherChangePresenter.new(@template, @planning_application,
-                                                other_change_validation_request).task_list_row
-    end
-
-    private
-
-    def validation_item_tag
-      classes = ["govuk-tag govuk-tag--#{validation_item_status_tag_colour} app-task-list__task-tag"]
-
-      tag.strong class: classes do
-        validation_item_status
-      end
-    end
-
-    def validation_item_status_tag_colour
-      STATUS_COLOURS[validation_item_status.to_sym]
     end
   end
 end
---
+++ spec/components/task_list_items/other_change_request_component_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe TaskListItems::OtherChangeRequestComponent, type: :component do
+  let(:planning_application) { create(:planning_application, :not_started) }
+
+  let(:other_change_validation_request) do
+    create(
+      :other_change_validation_request,
+      planning_application: planning_application,
+      sequence: 1,
+      state: :open
+    )
+  end
+
+  let(:component) do
+    described_class.new(
+      planning_application: planning_application,
+      request: other_change_validation_request
+    )
+  end
+
+  before { render_inline(component) }
+
+  it "renders 'Invalid' status" do
+    expect(page).to have_content("Invalid")
+  end
+
+  it "renders link" do
+    expect(page).to have_link(
+      "View other validation request #1",
+      href: "/planning_applications/#{planning_application.id}/other_change_validation_requests/#{other_change_validation_request.id}"
+    )
+  end
+
+  context "when request is closed" do
+    let(:other_change_validation_request) do
+      create(
+        :other_change_validation_request,
+        planning_application: planning_application,
+        sequence: 1,
+        state: :closed,
+        response: "response"
+      )
+    end
+
+    it "renders 'Updated' status" do
+      expect(page).to have_content("Updated")
+    end
+  end
+end
