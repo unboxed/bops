6baaa5e5f3e790b3202b9e81f01b6fb792a94dfb
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2023-01-09 14:31:42 +0000

    Increase minimum password length to 8 characters

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -33,7 +33,7 @@ Devise.setup do |config|
 
   config.expire_all_remember_me_on_sign_out = true
 
-  config.password_length = 6..128
+  config.password_length = 8..128
 
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
 
