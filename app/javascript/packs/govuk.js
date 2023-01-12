a4e801c52c13b9410e3cb8603ffe4d4986c43061
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2023-01-12 09:23:34 +0000

    Lint javascript

--- app/javascript/packs/govuk.js
+++ app/javascript/packs/govuk.js
@@ -3,7 +3,7 @@ var govukall = require("govuk-frontend/govuk/all")
 govukall.Accordion.prototype.originalsetExpanded = govukall.Accordion.prototype.setExpanded
 govukall.Accordion.prototype.setExpanded = function (expanded, $section) {
   this.originalsetExpanded(expanded, $section)
-  if (typeof accordion_expanded == 'function') { 
+  if (typeof accordion_expanded === 'function') { 
     accordion_expanded(); 
   }  
 }
