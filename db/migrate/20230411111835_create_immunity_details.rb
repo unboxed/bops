29ac8feed05870ae8af148e28bf2d6809a736263
Celia Collins <celiadcollins@gmail.com>
2023-04-11 12:19:36 +0100

    Add immunity detail table

---
+++ db/migrate/20230411111835_create_immunity_details.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateImmunityDetails < ActiveRecord::Migration[7.0]
+  def change
+    create_table :immunity_details do |t|
+      t.datetime :end_date
+      t.references :planning_application
+      t.string :status
+      t.timestamps
+    end
+  end
+end
