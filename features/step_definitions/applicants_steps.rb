a02d85573a8e925d02141208ca1d156d532f5cbf
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2023-04-11 14:41:27 +0100

    Fix Style/FormatStringToken

--- features/step_definitions/applicants_steps.rb
+++ features/step_definitions/applicants_steps.rb
@@ -5,9 +5,9 @@
 
 Given("I look at the validation requests on my application") do
   qs = format(
-    "planning_application_id=%s&change_access_id=%s",
-    @planning_application.id,
-    @planning_application.change_access_id
+    "planning_application_id=%<application_id>s&change_access_id=%<change_access_id>s",
+    application_id: @planning_application.id,
+    change_access_id: @planning_application.change_access_id
   )
 
   visit "/validation_requests/?#{qs}"
