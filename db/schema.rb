938fe94f92353dc0b9db6b64231bfc7a6ec2e2a6
Evangelos Giataganas <e.giataganas@gmail.com>
2023-04-12 17:11:54 +0300

    Drop delayed job table

--- db/schema.rb
+++ db/schema.rb
@@ -134,21 +134,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_103900) do
     t.index ["planning_application_id"], name: "ix_consultees_on_planning_application_id"
   end
 
-  create_table "delayed_jobs", force: :cascade do |t|
-    t.integer "priority", default: 0, null: false
-    t.integer "attempts", default: 0, null: false
-    t.text "handler", null: false
-    t.text "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string "locked_by"
-    t.string "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
-  end
-
   create_table "description_change_validation_requests", force: :cascade do |t|
     t.bigint "planning_application_id", null: false
     t.bigint "user_id", null: false
