dce32b1c90f07f7dcd6454b5d4d4fda30f9977a2
Celia Collins <celiadcollins@gmail.com>
2023-04-13 10:35:23 +0100

    Clean up using methods

--- app/presenters/concerns/assessment_tasks_presenter.rb
+++ app/presenters/concerns/assessment_tasks_presenter.rb
@@ -3,19 +3,21 @@
 module AssessmentTasksPresenter
   extend ActiveSupport::Concern
 
-  # rubocop:disable Metrics/AbcSize
   def assessment_tasklist_in_progress?
     policy_classes.any? ||
       consistency_checklist.present? ||
       assessment_details.any? ||
       permitted_development_right.present? ||
       recommendation.present? ||
-      (planning_application.immunity_detail.present? && planning_application.immunity_detail.status != "not_started")
-    ## Above line needs fixing when we add things that update the immunity details
+      immunity_validation_in_progress?
   end
-  # rubocop:enable Metrics/AbcSize
 
   def multiple_policy_classes?
     policy_classes.count > 1
+  end
+
+  def immunity_validation_in_progress?
+    planning_application.immunity_detail.present? && planning_application.immunity_detail.status != "not_started"
+    ## Above line needs fixing when we add things that update the immunity details
   end
 end
