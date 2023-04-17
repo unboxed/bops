38f94f75a2b2838efac1c689f765c681dd88f3d8
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2023-04-17 14:26:40 +0100

    Add documents to evidence group

--- app/models/immunity_detail.rb
+++ app/models/immunity_detail.rb
@@ -28,4 +28,12 @@ class ImmunityDetail < ApplicationRecord
   def update_required?
     complete? && !accepted
   end
+
+  def add_document(document)
+    tag = document.tags.intersection(Document::EVIDENCE_TAGS).first.delete(" ").underscore
+    evidence_group = evidence_groups.find_or_create_by(tag:)
+    evidence_group.documents << document
+    # TODO: update start/end date?
+    evidence_group.save!
+  end
 end
