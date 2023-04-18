3558a873f7fd6e8073bdc8435bed4dd855215fc0
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2023-04-18 11:26:43 +0100

    Add basic test for polymorphic comments

--- spec/models/evidence_group_spec.rb
+++ spec/models/evidence_group_spec.rb
@@ -3,5 +3,18 @@
 require "rails_helper"
 
 RSpec.describe EvidenceGroup do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#comments" do
+    let(:immunity_detail) { create(:immunity_detail) }
+    let(:document) { create(:document, tags: ["Photograph"]) }
+
+    before do
+      immunity_detail.add_document document
+    end
+
+    it "can have comments" do
+      immunity_detail.evidence_groups.first.comments << create(:comment)
+      expect(immunity_detail.evidence_groups.first.comments.first).not_to be_nil
+      expect(immunity_detail.evidence_groups.first.comments.first.commentable_type).to eq("EvidenceGroup")
+    end
+  end
 end
