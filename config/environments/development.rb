c0359066526d8c54c59653b236da88ee1a25d332
Celia Collins <celiadcollins@gmail.com>
2023-05-16 16:04:43 +0100

    Install bullet gem to help point out performance improvements

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
+
   # Settings specified here will take precedence over those in
   # config/application.rb.
 
