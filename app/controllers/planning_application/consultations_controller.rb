ae900c937cedaa7358c126f9989bbefe42e923e0
Celia Collins <celiadcollins@gmail.com>
2023-05-19 16:00:15 +0100

    Show basic map on consultations page

---
+++ app/controllers/planning_application/consultations_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class PlanningApplication
+  class ConsultationsController < AuthenticationController
+    before_action :set_planning_application
+
+    def new; end
+
+    private
+
+    def set_planning_application
+      planning_application = planning_applications_scope.find(planning_application_id)
+
+      @planning_application = PlanningApplicationPresenter.new(view_context, planning_application)
+    end
+
+    def planning_applications_scope
+      current_local_authority.planning_applications
+    end
+
+    def planning_application_id
+      Integer(params[:planning_application_id])
+    end
+  end
+end
