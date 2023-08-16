aeb572420e0e578e6d4cf7ec750a5cef7d609a71
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2023-08-16 10:05:47 +0100

    Render consultation start and end dates in key dates component

--- spec/components/accordion_sections/key_application_dates_component_spec.rb
+++ spec/components/accordion_sections/key_application_dates_component_spec.rb
@@ -50,8 +50,12 @@ RSpec.describe AccordionSections::KeyApplicationDatesComponent, type: :component
       render_inline(component)
     end
 
+    it "renders consultation start date" do
+      expect(page).to have_content("Consultation started:\n    #{consultation.start_date.to_date.to_fs}\n")
+    end
+
     it "renders consultation deadline" do
-      expect(page).to have_content("Consultation deadline:\n    #{consultation.end_date.strftime('%-d %B %Y')}")
+      expect(page).to have_content("Consultation deadline:\n    #{consultation.end_date.to_date.to_fs}\n")
     end
   end
 
@@ -73,6 +77,21 @@ RSpec.describe AccordionSections::KeyApplicationDatesComponent, type: :component
 
     it "renders consultation deadline as not started" do
       expect(page).to have_content("Consultation deadline:\n    Not yet started")
+    end
+  end
+
+  context "when the consultation has finished" do
+    let(:consultation) { create(:consultation, start_date: 4.weeks.ago, end_date: 1.week.ago) }
+
+    before do
+      planning_application.consultation = consultation
+      planning_application.save!
+
+      render_inline(component)
+    end
+
+    it "renders publicity end date" do
+      expect(page).to have_content("Consultation deadline:\n    #{consultation.end_date.to_date.to_fs}")
     end
   end
 end
