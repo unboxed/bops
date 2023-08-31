16a7d5d2356f356a7f122585ac85597e95c4ce1d
Celia Collins <celiadcollins@gmail.com>
2023-08-31 10:05:56 +0100

    Javascript linting

--- app/javascript/controllers/show_more_text_controller.js
+++ app/javascript/controllers/show_more_text_controller.js
@@ -6,11 +6,17 @@ export default class extends Controller {
     console.log("I am here")
 
     // Remove the ellipses from the comment
-    let replaceText = event.target.parentElement.getElementsByClassName("truncated-comment")[0].innerText.slice(0, -3)
-    event.target.parentElement.getElementsByClassName("truncated-comment")[0].innerText = replaceText
-   
+    let replaceText = event.target.parentElement
+      .getElementsByClassName("truncated-comment")[0]
+      .innerText.slice(0, -3)
+    event.target.parentElement.getElementsByClassName(
+      "truncated-comment",
+    )[0].innerText = replaceText
+
     // Show the rest of the comment and hide "show more"
-    event.target.parentElement.getElementsByClassName("hidden-comment display-none")[0].classList.remove("display-none")
+    event.target.parentElement
+      .getElementsByClassName("hidden-comment display-none")[0]
+      .classList.remove("display-none")
     event.target.classList.add("display-none")
   }
 }
