459d61a841395f99b14f2b73bda43088f419e9ba
Andrew White <andrew.white@unboxed.co>
2023-10-16 02:11:50 +0100

    Don't travel forward in time
    When travel_to is used with a date/time in the future it triggers the
    session tampering protection on Devise which logs out the user.

--- features/step_definitions/datetime_steps.rb
+++ features/step_definitions/datetime_steps.rb
@@ -18,15 +18,16 @@ end
 
 Given("the time is {int}:{int}") do |hour, minutes|
   now = Time.zone.now
+  destination = Time.zone.local(now.year, now.month, now.day, hour, minutes)
 
-  travel_to Time.zone.local(now.year, now.month, now.day, hour, minutes)
+  # Travelling forward in time will force the Devise session to expire
+  destination = destination.advance(days: -1) if destination.after?(now)
+
+  travel_to destination
 end
 
 Given("the time is {int}:{int} and the planning application is validated") do |hour, minutes|
-  now = Time.zone.now
-
-  travel_to Time.zone.local(now.year, now.month, now.day, hour, minutes)
-
+  steps("Given the time is #{hour}:#{minutes}")
   steps("Given the planning application is validated")
 
   visit root_path
