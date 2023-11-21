f8b09dd42784f9971d51b97d7742e7869c737261
Rhian <rhianatwork@gmail.com>
2023-11-21 21:21:21 +0000

    Migration to add Confirmable to Devise

---
+++ db/migrate/20231118113927_add_confirmed_at_to_devise.rb
@@ -0,0 +1,13 @@
+class AddConfirmedAtToDevise < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+
+    User.all.find_each do |user|
+      user.update(
+        confirmed_at: Time.zone.now
+      )
+    end
+  end
+end
