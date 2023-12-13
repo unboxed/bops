9a3f7a6e558396f1dc4a8a82352b95deefab1975
Andrew White <andrew.white@unboxed.co>
2023-12-13 05:05:57 +0000

    Undo schema changes in 8d09fdf49fe1eaf55f3ab70dc991fadf91a08a9c

--- db/schema.rb
+++ db/schema.rb
@@ -44,8 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_11_125913) do
   end
 
   create_table "api_users", force: :cascade do |t|
-    t.string "name"
-    t.string "token"
+    t.string "name", null: false
+    t.string "token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "local_authority_id"
@@ -501,12 +501,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_11_125913) do
     t.string "longitude"
     t.datetime "closed_at", precision: nil
     t.datetime "determination_date", precision: nil
-    t.boolean "updated_address_or_boundary_geojson", default: false
     t.integer "user_role"
+    t.boolean "updated_address_or_boundary_geojson", default: false
     t.boolean "constraints_checked", default: false, null: false
     t.boolean "valid_fee"
-    t.boolean "valid_red_line_boundary"
     t.boolean "documents_missing"
+    t.boolean "valid_red_line_boundary"
     t.decimal "invalid_payment_amount", precision: 10, scale: 2
     t.bigint "application_number", null: false
     t.string "parish_name"
