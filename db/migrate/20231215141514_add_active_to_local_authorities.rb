0f3258019abf2cb0d95cf9f6ee077716015a70bf
Rhian <rhianatwork@gmail.com>
2023-12-19 14:41:45 +0000

    Add Planx to local authorities list for migration

--- db/migrate/20231215141514_add_active_to_local_authorities.rb
+++ db/migrate/20231215141514_add_active_to_local_authorities.rb
@@ -4,7 +4,7 @@ class AddActiveToLocalAuthorities < ActiveRecord::Migration[7.0]
   def up
     add_column :local_authorities, :active, :boolean, null: false, default: false
 
-    active_authorities = %w[southwark lambeth buckinghamshire camden]
+    active_authorities = %w[southwark lambeth buckinghamshire camden plan_x]
 
     LocalAuthority.find_each do |authority|
       authority.update(active: true) if active_authorities.include?(authority.subdomain)
