ab373567eea6e66d9601b609e15b8ea586af746a
Celia Collins <celiadcollins@gmail.com>
2024-01-05 16:35:49 +0000

    Add template toggle

--- app/javascript/controllers/neighbours_controller.js
+++ app/javascript/controllers/neighbours_controller.js
@@ -14,4 +14,17 @@ export default class extends Controller {
       "input[type=checkbox]",
     )
   }
+
+  toggleTemplates(event) {
+    console.log("i am here")
+    const template = event.target.value
+
+    const target = document.getElementById("renotification-form")
+
+    if(template === "Renotification") {
+      target.classList.remove("govuk-!-display-none")
+    } else {
+      target.classList.add("govuk-!-display-none")
+    }
+  }
 }
