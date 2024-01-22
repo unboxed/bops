ad34e658647950f55082bead4675340ac3bf20ad
Andrew White <andrew.white@unboxed.co>
2024-01-22 12:12:00 +0000

    Only load English locale files
    Some gems have alternative locales so don't load those unnecessarily.

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module Bops
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :en
+    config.i18n.available_locales = %i[en]
 
     # Don't generate system test files.
     config.generators.system_tests = nil
