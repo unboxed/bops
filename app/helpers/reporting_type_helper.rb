1ee46c8d20b49e476dee01f0e45e060b2b685dae
Evangelos Giataganas <e.giataganas@gmail.com>
2024-01-24 23:55:10 +0200

    Use key for category

--- app/helpers/reporting_type_helper.rb
+++ app/helpers/reporting_type_helper.rb
@@ -3,7 +3,7 @@
 module ReportingTypeHelper
   def reporting_types(application_type)
     I18n.t(application_type, scope: :reporting_type).flat_map do |type, information|
-      [information.values]
+      [[type.to_s] + information.values]
     end
   end
 end
