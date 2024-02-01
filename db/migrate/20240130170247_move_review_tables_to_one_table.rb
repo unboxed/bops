99e7aa756213e8ba853dd6d285819d1afc173c67
Celia Collins <celiadcollins@gmail.com>
2024-02-01 16:33:29 +0000

    Write down migration

--- db/migrate/20240130170247_move_review_tables_to_one_table.rb
+++ db/migrate/20240130170247_move_review_tables_to_one_table.rb
@@ -5,6 +5,28 @@ class MoveReviewTablesToOneTable < ActiveRecord::Migration[7.1]
     store_accessor :specific_attributes, %w[decision decision_reason summary decision_type removed review_type]
     belongs_to :reviewable, polymorphic: true, optional: true
     belongs_to :owner, polymorphic: true
+
+    with_options class_name: "User", optional: true do
+      belongs_to :assessor
+      belongs_to :reviewer
+    end
+  end
+
+  class ReviewLocalPolicy < ApplicationRecord
+    with_options class_name: "User", optional: true do
+      belongs_to :assessor
+      belongs_to :reviewer
+    end
+  end
+
+  class ReviewPolicyClass < ApplicationRecord
+  end
+
+  class ReviewImmunityDetail < ApplicationRecord
+    with_options class_name: "User", optional: true do
+      belongs_to :assessor
+      belongs_to :reviewer
+    end
   end
 
   def up
@@ -78,5 +100,105 @@ class MoveReviewTablesToOneTable < ActiveRecord::Migration[7.1]
   end
 
   def down
+    create_table "review_policy_classes", force: :cascade do |t|
+      t.references :policy_class, null: false
+      t.integer :mark, null: false
+      t.string :comment
+      t.integer :status, null: false
+      t.timestamps
+    end
+
+    create_table "review_local_policies", force: :cascade do |t|
+      t.references :assessor, null: false, foreign_key: {to_table: :users}
+      t.references :reviewer, foreign_key: {to_table: :users}
+      t.boolean :accepted, default: false, null: false
+      t.string :status, default: "in_progress", null: false
+      t.string :review_status, default: "review_not_started", null: false
+      t.boolean :reviewer_edited, default: false, null: false
+      t.text :reviewer_comment
+      t.datetime :reviewed_at
+      t.references :local_policy
+      t.timestamps
+    end
+
+    create_table "review_immunity_details", force: :cascade do |t|
+      t.references :immunity_detail
+      t.references :assessor, null: false, foreign_key: {to_table: :users}
+      t.references :reviewer, foreign_key: {to_table: :users}
+      t.string :decision
+      t.text :decision_reason
+      t.text :summary
+      t.boolean :accepted, default: false, null: false
+      t.text :reviewer_comment
+      t.datetime :reviewed_at
+      t.string :decision_type
+      t.string :status, default: "in_progress", null: false
+      t.boolean :removed, null: false, default: false
+      t.boolean :reviewer_edited, default: false, null: false
+      t.string :review_status, default: "review_not_started", null: false
+      t.string :review_type, default: "enforcement", null: false
+      t.timestamps
+    end
+
+    Review.find_each do |review|
+      case review.owner_type
+      when "LocalPolicy"
+        rlp = ReviewLocalPolicy.create!(
+          assessor_id: review.assessor.id,
+          reviewer_id: review.reviewer&.id,
+          accepted: if review.action.nil?
+                      nil
+                    else
+                      review.action == "accepted"
+                    end,
+          status: review.status,
+          review_status: review.review_status,
+          reviewer_edited: review.reviewer_edited,
+          reviewer_comment: review.comment,
+          reviewed_at: review.reviewed_at,
+          local_policy_id: review.owner.id
+        )
+        rlp.update!(created_at: review.created_at, updated_at: review.updated_at)
+      when "ImmunityDetail"
+        rid = ReviewImmunityDetail.create!(
+          immunity_detail_id: review.owner.id,
+          assessor_id: review.assessor.id,
+          reviewer_id: review.reviewer&.id,
+          decision: review.specific_attributes["decision"],
+          decision_reason: review.specific_attributes["decision_reason"],
+          decision_type: review.specific_attributes["decision_type"],
+          summary: review.specific_attributes["summary"],
+          review_type: review.specific_attributes["review_type"],
+          removed: review.specific_attributes["removed"],
+          accepted: if review.action.nil?
+                      nil
+                    else
+                      review.action == "accepted"
+                    end,
+          reviewer_comment: review.comment,
+          reviewed_at: review.reviewed_at,
+          status: review.status,
+          review_status: review.review_status,
+          reviewer_edited: review.reviewer_edited
+        )
+        rid.update!(created_at: review.created_at, updated_at: review.updated_at)
+      when "PolicyClass"
+        rpc = ReviewPolicyClass.create!(
+          policy_class_id: review.owner.id,
+          mark: review.action,
+          comment: review.comment,
+          status: review.status
+        )
+        rpc.update!(created_at: review.created_at, updated_at: review.updated_at)
+      end
+    end
+
+    change_table :reviews, bulk: true do |t|
+      t.remove :review_status
+      t.remove :reviewer_edited
+      t.remove :specific_attributes
+      t.rename :owner_type, :reviewable_type
+      t.rename :owner_id, :reviewable_id
+    end
   end
 end
