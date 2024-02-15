653ccba5178e04513903a7eb92a61d4d953a9726
Andrew White <andrew.white@unboxed.co>
2024-02-15 14:51:58 +0000

    Remove unnecessary callbacks and database fetches

--- app/controllers/planning_applications/assessment/tasks_controller.rb
+++ app/controllers/planning_applications/assessment/tasks_controller.rb
@@ -4,18 +4,11 @@ module PlanningApplications
   module Assessment
     class TasksController < AuthenticationController
       before_action :set_planning_application
-      before_action :set_condition_set
 
       def index
         respond_to do |format|
           format.html
         end
-      end
-
-      private
-
-      def set_condition_set
-        @condition_set = @planning_application.condition_set
       end
     end
   end
