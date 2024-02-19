db47c657c623d5e13e8689e60c4095671bd216cf
Andrew White <andrew.white@unboxed.co>
2024-02-19 15:26:17 +0000

    Retry PlanX queries that time out
    Faraday 5xx errors are reported to Appsignal but they're mostly timeouts
    so just retry the job a few times rather that spamming the Slack channel.

--- app/jobs/constraint_query_update_job.rb
+++ app/jobs/constraint_query_update_job.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+require "faraday"
+
 class ConstraintQueryUpdateJob < ApplicationJob
   queue_as :high_priority
+  retry_on Faraday::TimeoutError, wait: 5.minutes, jitter: 0
 
   def perform(planning_application:)
     ConstraintQueryUpdateService.new(
