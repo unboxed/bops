c1a22c690fd94a608bd13135eac3320afa5c5fd7
Andrew White <andrew.white@unboxed.co>
2024-02-21 10:58:29 +0000

    Extract callbacks to a concern for reuse

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,31 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
-
-  # Most jobs are safe to ignore if the underlying records
-  # are no longer available discard_on ActiveJob::DeserializationError
-
-  before_enqueue do |job|
-    next unless Current.user
-
-    if job.arguments.last.is_a?(Hash)
-      job.arguments.last[:current_user] = Current.user
-    else
-      job.arguments << {current_user: Current.user}
-    end
-  end
-
-  before_perform do |job|
-    next unless job.arguments.last.is_a?(Hash)
-
-    if job.arguments.last.key?(:current_user)
-      Current.user = job.arguments.last.delete(:current_user)
-
-      if job.arguments.last.empty?
-        job.arguments.pop
-      end
-    end
-  end
+  include CurrentUserForJob
 end
