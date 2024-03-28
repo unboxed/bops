8c1d30503c7b89865834be9cbc1a3071fea70722
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-03-28 11:52:53 +0000

    Make condition text less herbal

--- spec/factories/condition.rb
+++ spec/factories/condition.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :condition do
     title { "Time limit" }
-    text { "The development herby permitted shall be commenced within three years of the date of this permission." }
+    text { "The development hereby permitted shall be commenced within three years of the date of this permission." }
     reason { "To comply with the provisions of Section 91 of the Town and Country Planning Act 1990 (as amended)." }
     standard { true }
 
