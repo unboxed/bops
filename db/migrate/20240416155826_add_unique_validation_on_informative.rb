714c41e71eb516932707a9c62db073862f9bed43
Celia Collins <celiadcollins@gmail.com>
2024-04-18 15:48:48 +0100

    Use if_not_exists when disabling ddl transaction

--- db/migrate/20240416155826_add_unique_validation_on_informative.rb
+++ db/migrate/20240416155826_add_unique_validation_on_informative.rb
@@ -4,7 +4,7 @@ class AddUniqueValidationOnInformative < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
-    add_index :informatives, [:title, :informative_set_id], unique: true, algorithm: :concurrently
-    add_index :informatives, [:text, :informative_set_id], unique: true, algorithm: :concurrently
+    add_index :informatives, [:title, :informative_set_id], unique: true, algorithm: :concurrently, if_not_exists: true
+    add_index :informatives, [:text, :informative_set_id], unique: true, algorithm: :concurrently, if_not_exists: true
   end
 end
