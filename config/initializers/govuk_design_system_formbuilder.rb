7de82d0d347bfb28f3a0903c9a6d068b6d9627f1
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-04-29 10:51:32 +0100

    Set ErrorPresenter as default

--- config/initializers/govuk_design_system_formbuilder.rb
+++ config/initializers/govuk_design_system_formbuilder.rb
@@ -4,4 +4,8 @@ require "govuk_design_system_formbuilder"
 
 GOVUKDesignSystemFormBuilder.configure do |conf|
   conf.default_submit_button_text = "Save"
+
+  Rails.application.config.to_prepare do
+    conf.default_error_summary_presenter = ErrorPresenter
+  end
 end
