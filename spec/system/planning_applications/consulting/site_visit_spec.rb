2489b8e1f6a8c3ecd0ac003fb4a3b201ada91ad4
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-04-30 10:47:22 +0100

    Do not require comment if site visit not performed

--- spec/system/planning_applications/consulting/site_visit_spec.rb
+++ spec/system/planning_applications/consulting/site_visit_spec.rb
@@ -137,17 +137,22 @@ RSpec.describe "Site visit" do
       click_button "Save"
 
       within(".govuk-error-summary") do
-        expect(page).to have_content("Enter a comment about the site visit")
         expect(page).to have_content("Choose 'Yes' or 'No'")
       end
       within("#site-visit-decision-error") do
         expect(page).to have_content("Choose 'Yes' or 'No'")
       end
+
+      choose "Yes"
+      click_button("Save")
+
+      within(".govuk-error-summary") do
+        expect(page).to have_content("Enter a comment about the site visit")
+      end
       within("#site-visit-comment-error") do
         expect(page).to have_content("Enter a comment about the site visit")
       end
 
-      choose "Yes"
       attach_file("Upload photo(s)", "spec/fixtures/images/image.gif")
       click_button("Save")
 
@@ -278,6 +283,15 @@ RSpec.describe "Site visit" do
         expect(page).to have_content("Comment: Site visit not needed")
 
         expect(page).not_to have_css(".govuk-table")
+      end
+
+      it "I do not have give a reason why if I choose no" do
+        choose "No"
+        click_button "Save"
+
+        within("#site-visit") do
+          expect(page).to have_content("Completed")
+        end
       end
     end
   end
