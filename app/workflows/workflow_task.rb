c93ebbcfa6ba59abb5ec0032fbbe6806255425fe
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-05-02 16:34:26 +0100

    Convert validation tasks to govuk-components task list

---
+++ app/workflows/workflow_task.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class WorkflowTask
+  include Rails.application.routes.url_helpers
+
+  def initialize(planning_application)
+    @planning_application = planning_application
+    @task_list_link_text = nil
+    @task_list_link = nil
+    @task_list_status = :unknown
+  end
+
+  attr_reader :task_list_link_text, :task_list_link, :task_list_status
+
+  private
+
+  attr_reader :planning_application
+end
