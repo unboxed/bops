73954509b278978914530ca79da382a7c5cb0a94
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-05-03 15:35:05 +0100

    Move letter sending statuses into a subcategory

--- app/components/status_tags/letter_component.rb
+++ app/components/status_tags/letter_component.rb
@@ -3,8 +3,29 @@
 module StatusTags
   class LetterComponent < StatusTags::BaseComponent
     def initialize(status:)
-      @status = status
+      @status = status.to_sym
+      raise "Invalid status `#{@status.inspect}'" unless NeighbourLetter::STATUSES.values.map(&:to_sym).include?(@status) || @status == :new
+
       super(status:)
+    end
+
+    def link_text
+      t("status_tag_component.letter_status.#{status}")
+    end
+
+    def colour
+      return "red" if NeighbourLetter::FAILURE_STATUSES.include? status
+
+      case status.to_sym
+      when :new
+        "blue"
+      when :posted
+        "light-blue"
+      when :submitted
+        "yellow"
+      when :printing
+        "purple"
+      end
     end
   end
 end
