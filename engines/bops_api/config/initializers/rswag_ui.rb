288112ca0f80ec0cf9dec4c348c7369b162c07fb
Andrew White <andrew.white@unboxed.co>
2024-05-08 14:59:16 +0100

    Use new request-snippets plugin and set to hidden by default

--- engines/bops_api/config/initializers/rswag_ui.rb
+++ engines/bops_api/config/initializers/rswag_ui.rb
@@ -4,6 +4,11 @@ Rswag::Ui.configure do |c|
   c.openapi_endpoint "/api/docs/v2/swagger_doc.yaml", "API V2 Docs"
 
   c.config_object.merge!(
-    persistAuthorization: true
+    persistAuthorization: true,
+    requestSnippetsEnabled: true,
+    requestSnippets: {
+      defaultExpanded: false,
+      languages: ["curl_bash"]
+    }
   )
 end
