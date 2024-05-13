a649e245301370f70ed01ba07f32c9c08fc3ce50
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-05-13 15:15:01 +0100

    Replace final inline task list items with task classes

---
+++ app/workflows/validation/add_validation_request_task.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Validation
+  class AddValidationRequestTask < WorkflowTask
+    def task_list_link_text
+      "Add another validation request"
+    end
+
+    def task_list_link
+      new_planning_application_validation_validation_request_path(planning_application.id, type: :other_change)
+    end
+  end
+end
