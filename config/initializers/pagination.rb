a3230e0374e1ebe43aabc767e266027a2261cc2a
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-05-17 14:46:59 +0100

    Match Pagy component to GOV.UK standard

---
+++ config/initializers/pagination.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Match GOV.UK Design System
+# https://govuk-components.netlify.app/components/pagination/#when-there-are-lots-of-pages
+Pagy::DEFAULT[:size] = [1, 1, 1, 1]
