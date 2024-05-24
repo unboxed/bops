3dac4809ba78f40dcc7fbee1dd2c88d866c2e601
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-05-24 13:26:37 +0100

    Update other ShowMoreTextController uses

--- app/javascript/controllers/show_more_text_controller.js
+++ app/javascript/controllers/show_more_text_controller.js
@@ -1,24 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  handleClick(event) {
-    event.preventDefault()
-
-    const truncatedComment =
-      this.element.getElementsByClassName("truncated-comment")[0]
-    const hiddenComment =
-      this.element.getElementsByClassName("hidden-comment")[0]
-
-    // Remove the ellipses from the comment
-    const replaceText = truncatedComment.innerText.slice(0, -3)
-
-    truncatedComment.innerText = replaceText
-
-    truncatedComment.innerHTML += hiddenComment.innerHTML
-
-    event.target.classList.add("govuk-!-display-none")
-  }
-
   replaceText(event) {
     event.preventDefault()
 
