39414f8fc455d8414eb78ea611cc704d7f168375
Andrew White <andrew.white@unboxed.co>
2024-05-29 19:03:19 +0100

    Move planning guidance url to the local authority record

--- db/schema.rb
+++ db/schema.rb
@@ -455,6 +455,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_135942) do
     t.boolean "active", default: false, null: false
     t.string "telephone_number"
     t.string "document_checklist"
+    t.string "planning_policy_and_guidance"
     t.index ["subdomain"], name: "index_local_authorities_on_subdomain", unique: true
   end
 
