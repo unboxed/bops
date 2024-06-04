c2383d1704a4cc604fc36e19ae024e692efa74f8
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-06-04 12:30:28 +0100

    Remove an unused helper

--- app/components/evidence_groups/documents_component.rb
+++ app/components/evidence_groups/documents_component.rb
@@ -7,15 +7,5 @@ module EvidenceGroups
     end
 
     attr_reader :documents
-
-    private
-
-    def url_for_document(document)
-      if document.published?
-        api_v1_planning_application_document_url(document.planning_application, document)
-      else
-        rails_blob_url(document.file)
-      end
-    end
   end
 end
