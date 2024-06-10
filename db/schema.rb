a1632d0da1900afe9ecd397d5f43a99ed7197db6
Evangelos Giataganas <e.giataganas@gmail.com>
2024-06-10 13:45:09 +0300

    Update Conditions cancelled_at values
    When there's a cancelled validation request update conditions
    cancelled_at timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_150320) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_10_102853) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "plpgsql"
