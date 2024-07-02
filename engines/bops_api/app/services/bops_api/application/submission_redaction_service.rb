6907b803ae2ab314a27dbb237fcc24c6bca093e1
Ben Baumann <34001723+benbaumann95@users.noreply.github.com>
2024-07-02 16:26:31 +0100

    Redact fee and contact info in responses

--- engines/bops_api/app/services/bops_api/application/submission_redaction_service.rb
+++ engines/bops_api/app/services/bops_api/application/submission_redaction_service.rb
@@ -16,19 +16,28 @@ module BopsApi
         ["data", "applicant", "agent", "email"]
       ]
 
+      RESPONSES_TO_REDACT = [
+        "Applicant's contact details",
+        "Your contact details",
+        "Applicant's email address",
+        "Applicant's telephone number"
+      ]
+
       def initialize(planning_application:)
         @planning_application = planning_application
       end
 
       def call
-        return unless (submission = @planning_application.params_v2)
+        return unless (submission = planning_application.params_v2)
 
-        redact_fields(submission)
+        redact_fields(redact_responses(submission))
       rescue => e
         raise RedactionError, e.message
       end
 
       private
+
+      attr_reader :planning_application
 
       def redact_fields(data)
         FIELDS_TO_REDACT.each do |field_path|
@@ -36,7 +45,18 @@ module BopsApi
           field&.replace(REDACTION_TEXT)
         end
 
+        data["data"]["application"]["fee"] = REDACTION_TEXT
         data
+      end
+
+      def redact_responses(submission)
+        submission["responses"].each do |response|
+          if RESPONSES_TO_REDACT.include?(response["question"])
+            response["responses"] = [{"value" => REDACTION_TEXT}]
+          end
+        end
+
+        submission
       end
     end
   end
