e937c67ad873aa262311d3b2dd669609eff92e4e
Ben Baumann <34001723+benbaumann95@users.noreply.github.com>
2024-07-04 11:00:25 +0100

    Set cors headers for public API

--- engines/bops_api/app/controllers/bops_api/v2/public_controller.rb
+++ engines/bops_api/app/controllers/bops_api/v2/public_controller.rb
@@ -3,6 +3,17 @@
 module BopsApi
   module V2
     class PublicController < ApplicationController
+      before_action :set_cors_headers
+
+      def set_cors_headers
+        response.set_header("Access-Control-Allow-Origin", "*")
+        response.set_header("Access-Control-Allow-Methods", "GET")
+        response.set_header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        )
+        response.charset = "utf-8"
+      end
     end
   end
 end
