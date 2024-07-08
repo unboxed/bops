458cd752dca5e67b769e08165e28beb504bc3758
Evangelos Giataganas <e.giataganas@gmail.com>
2024-07-08 17:07:08 +0300

    Include document tags in redacted documents

--- app/controllers/planning_applications/validation/document/redactions_controller.rb
+++ app/controllers/planning_applications/validation/document/redactions_controller.rb
@@ -13,6 +13,7 @@ module PlanningApplications
         end
 
         def create
+          filter_tags!
           @planning_application.assign_attributes(planning_application_params)
 
           respond_to do |format|
@@ -34,7 +35,17 @@ module PlanningApplications
         end
 
         def documents_params
-          %i[file redacted publishable validated]
+          [:file, :redacted, :publishable, :validated, tags: []]
+        end
+
+        def filter_tags!
+          return unless params[:planning_application][:documents_attributes]
+
+          params[:planning_application][:documents_attributes].each do |index, document_attributes|
+            if document_attributes[:tags].is_a?(String)
+              params[:planning_application][:documents_attributes][index][:tags] = document_attributes[:tags].split
+            end
+          end
         end
       end
     end
