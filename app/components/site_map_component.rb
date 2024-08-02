d5c85e45a78525be9378a022fcc7cd7e3312c31a
Evangelos Giataganas <e.giataganas@gmail.com>
2024-08-02 17:20:55 +0300

    Redesign reviewer landing page

---
+++ app/components/site_map_component.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class SiteMapComponent < ViewComponent::Base
+  def initialize(planning_application:)
+    @planning_application = planning_application
+  end
+
+  private
+
+  attr_reader :planning_application
+
+  private
+
+  def site_map_drawn_by
+    planning_application.boundary_created_by&.name || t(".applicant")
+  end
+
+  def change_request_link_path
+    case change_request&.state
+    when "open", "closed"
+      planning_application_validation_validation_request_path(
+        planning_application,
+        change_request
+      )
+    else
+      new_planning_application_validation_validation_request_path(
+        planning_application,
+        type: "red_line_boundary_change"
+      )
+    end
+  end
+
+  def change_request_link_text
+    case change_request&.state
+    when "open"
+      t(".view_requested_red")
+    when "closed"
+      t(".view_applicants_response")
+    else
+      t(".request_approval_for")
+    end
+  end
+
+  def change_request
+    @change_request ||= planning_application
+      .red_line_boundary_change_validation_requests
+      .post_validation
+      .last
+  end
+end
