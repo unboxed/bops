2ea63e2f1ace60bb5fb9f16c7f223726337233c9
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-08-12 11:19:52 +0100

    Set up for passing multiple layers to map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -4,24 +4,37 @@ import "leaflet-css"
 
 export default class extends Controller {
   connect() {
+    const layerData = JSON.parse(this.element.dataset.layers)
+    const redline = layerData.redline
+
+    const layers = []
+
     const [lat, long, ..._] = this.element.dataset.latlong.split(",")
+
     this.map = L.map(this.element.id, {
       center: [lat, long],
-      zoom: 12,
+      zoom: 17,
     })
+
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       maxZoom: 25,
       attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(this.map)
 
-    const redline = JSON.parse(this.element.dataset.redline)
-    L.geoJson(redline, {
-      style: {
-        color: "#ff0000",
-        weight: 2,
-        opacity: 0.67,
-      },
-    }).addTo(this.map)
+    if (redline !== null) {
+      const geoJsonLayer = L.geoJson(redline, {
+        style: {
+          color: "#ff0000",
+          weight: 2,
+          opacity: 0.67,
+        },
+      })
+      layers.push(geoJsonLayer)
+    }
+
+    for (const layer of layers) {
+      layer.addTo(this.map)
+    }
   }
 }
