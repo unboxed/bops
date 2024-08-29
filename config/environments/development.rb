a7f1ea0b8a50011703323ce32bda639e84848e47
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-08-29 16:15:14 +0100

    Enable annotate_rendered_view_with_filenames in dev
    This gets enabled by default in 7.2 but has been an option since before
    then and is nice to have when debugging views.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,6 +77,9 @@ Rails.application.configure do
   # Raises error for missing translations.
   config.i18n.raise_on_missing_translations = true
 
+  # Annotate rendered view with file names.
+  config.action_view.annotate_rendered_view_with_filenames = true
+
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
