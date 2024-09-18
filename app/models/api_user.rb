f77d4b0e3d37b5a51db23bdfecf13e5de4234166
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-09-18 12:17:51 +0100

    Limit api users to unrevoked by default

--- app/models/api_user.rb
+++ app/models/api_user.rb
@@ -8,12 +8,15 @@ class ApiUser < ApplicationRecord
 
   has_secure_token :token, length: 36
 
+  scope :active, -> { where(revoked_at: nil) }
+  scope :revoked, -> { where.not(revoked_at: nil) }
+
   validates :name, presence: true, uniqueness: {scope: :local_authority_id}
   validates :file_downloader, store_model: {allow_blank: true}
 
   class << self
     def authenticate(token)
-      find_by(token: token)
+      active.find_by(token: token)
     end
   end
 end
