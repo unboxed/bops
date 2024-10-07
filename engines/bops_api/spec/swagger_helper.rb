c1d6b42a803f10a89d61f527f0aae0f5b8e8417c
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2024-10-07 09:22:15 +0100

    Don't do strict validation

--- engines/bops_api/spec/swagger_helper.rb
+++ engines/bops_api/spec/swagger_helper.rb
@@ -6,7 +6,7 @@ require "rswag/specs"
 Dir[BopsApi::Engine.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
-  config.openapi_strict_schema_validation = true
+  config.openapi_no_additional_properties = true if config.respond_to? :openapi_no_additional_properties
   config.openapi_root = BopsApi::Engine.root.join("swagger").to_s
   config.openapi_format = :yaml
 
