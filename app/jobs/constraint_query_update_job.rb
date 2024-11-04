2154f34e58598a8c5e3ac520915ab1144333f775
Ben Baumann <34001723+benbaumann95@users.noreply.github.com>
2024-11-04 14:12:45 +0000

    Block is yielded with error instance as the second parameter
    https://api.rubyonrails.org/classes/ActiveJob/Exceptions/ClassMethods.html#method-i-retry_on

--- app/jobs/constraint_query_update_job.rb
+++ app/jobs/constraint_query_update_job.rb
@@ -5,8 +5,8 @@ require "faraday"
 class ConstraintQueryUpdateJob < ApplicationJob
   queue_as :high_priority
 
-  retry_on(Faraday::TimeoutError, attempts: 12, wait: 5.minutes, jitter: 0) do |error|
-    Appsignal.send_exception(error)
+  retry_on(Faraday::TimeoutError, attempts: 12, wait: 5.minutes, jitter: 0) do |_, error|
+    Appsignal.report_error(error)
   end
 
   def perform(planning_application:)
