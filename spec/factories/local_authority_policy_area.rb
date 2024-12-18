f89956f527a68c36f4103fc598aac6ff722d0989
Andrew White <andrew.white@unboxed.co>
2024-12-18 17:25:12 +0000

    Use strings for nested model factory class names
    Using the actual class name triggers the model to load in situations where
    this is undesirable such as when creating the database during CI runs.

--- spec/factories/local_authority_policy_area.rb
+++ spec/factories/local_authority_policy_area.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :local_authority_policy_area, class: LocalAuthority::PolicyArea do
+  factory :local_authority_policy_area, class: "LocalAuthority::PolicyArea" do
     local_authority
     description { Faker::Lorem.unique.sentence }
   end
--- spec/factories/local_authority_policy_reference.rb
+++ spec/factories/local_authority_policy_reference.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :local_authority_policy_reference, class: LocalAuthority::PolicyReference do
+  factory :local_authority_policy_reference, class: "LocalAuthority::PolicyReference" do
     local_authority
     code { Faker::IDNumber.unique.ssn_valid }
     description { Faker::Lorem.unique.sentence }
