9418c1b9a8248918cf30970db19f47b33e1236cf
Ben Baumann <34001723+benbaumann95@users.noreply.github.com>
2025-01-14 10:15:37 +0000

    Use global signed ID for magic link auth
    - Handle valid and invalid sgid in magic link
    - Handle expired resource so that magic link user can request a new link if previous link has expired
    - Add magic link mailer for consultee

--- engines/bops_consultees/app/controllers/bops_consultees/application_controller.rb
+++ engines/bops_consultees/app/controllers/bops_consultees/application_controller.rb
@@ -3,6 +3,9 @@
 module BopsConsultees
   class ApplicationController < ActionController::Base
     include BopsCore::ApplicationController
+    include BopsCore::MagicLinkAuthenticatable
+
+    before_action :authenticate_with_sgid!
 
     layout "application"
   end
---
+++ engines/bops_core/app/models/concerns/bops_core/magic_linkable.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module BopsCore
+  module MagicLinkable
+    extend ActiveSupport::Concern
+    include GlobalID::Identification
+
+    def sgid(expires_in: 48.hours, for: "magic_link")
+      to_sgid(expires_in:, for:).to_s
+    end
+  end
+end
