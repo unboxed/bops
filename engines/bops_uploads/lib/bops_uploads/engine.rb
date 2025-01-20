51107a1471b49e606b4e1b5f66594d96b6a4a4e0
Andrew White <andrew.white@unboxed.co>
2025-01-20 10:02:31 +0000

    Reverse document lookup
    Finding the document by using joins is horrendously slow so
    walk back up the association hierarchy using recursion until
    we hit a non-ActiveStorage record.

--- engines/bops_uploads/lib/bops_uploads/engine.rb
+++ engines/bops_uploads/lib/bops_uploads/engine.rb
@@ -6,7 +6,7 @@ module BopsUploads
   class Engine < ::Rails::Engine
     isolate_namespace BopsUploads
 
-    initializer "bops_uploads.cookie_signer" do |app|
+    initializer "bops_uploads.cookie_signer" do
       config.after_initialize do
         key_pair_id = ENV.fetch("UPLOADS_PUBLIC_KEY_ID") do
           SecureRandom.alphanumeric(14).upcase
@@ -21,6 +21,23 @@ module BopsUploads
         BopsUploads.key_pair_id = key_pair_id
         BopsUploads.private_key = OpenSSL::PKey::RSA.new(private_key)
         BopsUploads.cookie_signer = cookie_signer
+      end
+    end
+
+    initializer "bops_uploads.parent_record" do
+      ActiveSupport.on_load(:active_storage_blob) do
+        def parent_record(record = attachments.sole.record)
+          case record
+          when ActiveStorage::Blob
+            parent_record(record.attachments.sole.record)
+          when ActiveStorage::VariantRecord
+            parent_record(record.blob.attachments.sole.record)
+          else
+            record
+          end
+        end
+
+        alias_method :document, :parent_record
       end
     end
   end
