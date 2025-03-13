3d1978bc51daa8b64f0b0cf8e6bb7782219f669b
Evangelos Giataganas <e.giataganas@gmail.com>
2025-03-13 14:16:33 +0200

    Create local authority application_types dynamically

--- engines/bops_api/app/services/bops_api/application/parsers/application_type_parser.rb
+++ engines/bops_api/app/services/bops_api/application/parsers/application_type_parser.rb
@@ -12,7 +12,18 @@ module BopsApi
 
         def application_type
           config = ApplicationType::Config.find_by!(code: params[:value])
-          local_authority.application_types.find_by!(config_id: config.id)
+          retried = false
+
+          begin
+            local_authority.application_types.find_or_create_by!(config_id: config.id, code: config.code, name: config.name, suffix: config.suffix)
+          rescue ActiveRecord::RecordNotUnique
+            if retried
+              raise "Unable find or create application type"
+            else
+              retried = true
+              retry
+            end
+          end
         end
       end
     end
