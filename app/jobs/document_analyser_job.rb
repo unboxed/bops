1754d7ce6ea0bbee717b88be26895989d0e0549e
Ben Baumann <34001723+benbaumann95@users.noreply.github.com>
2025-03-19 11:54:51 +0000

    Add openai integration for analysing documents

---
+++ app/jobs/document_analyser_job.rb
@@ -0,0 +1,8 @@
+class DocumentAnalyserJob < ApplicationJob
+  queue_as :high_priority
+  retry_on(StandardError, attempts: 5, wait: 1.minute, jitter: 0)
+
+  def perform(document)
+    DocumentAnalyserService.new(document).call
+  end
+end
