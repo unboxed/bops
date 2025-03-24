d22c7ae6b481037ec708d2342fb383dfd396483f
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2025-03-24 10:18:29 +0000

    Refactor verify-language-versions.sh

--- bin/verify-language-versions.sh
+++ bin/verify-language-versions.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 set -e
 
-NODE_VERSION=$(cat .node-version)
-if ! grep -q "^ARG NODE_VERSION=$NODE_VERSION" docker/ruby/Dockerfile; then
-	echo "Dockerfile has wrong node: $(grep '^ARG NODE_MAJOR' docker/ruby/Dockerfile)" >&2
-	exit 1
-fi
-if ! grep -q "^ARG NODE_VERSION=$NODE_VERSION" Dockerfile.production; then
-	echo "Dockerfile.production has wrong node: $(grep '^ARG NODE_MAJOR' Dockerfile.production)" >&2
-	exit 1
-fi
+EXIT=0
 
+NODE_VERSION=$(cat .node-version)
 RUBY_VERSION=$(cat .ruby-version)
-if ! grep -q "^ARG RUBY_VERSION=$RUBY_VERSION" docker/ruby/Dockerfile; then
-	echo "Dockerfile has wrong ruby: $(grep '^FROM ruby:' docker/ruby/Dockerfile)" >&2
-	exit 1
-fi
-if ! grep -q "^ARG RUBY_VERSION=$RUBY_VERSION" Dockerfile.production; then
-	echo "Dockerfile.production has wrong ruby: $(grep '^ARG RUBY_VERSION' Dockerfile.production)" >&2
-	exit 1
-fi
+
+for dockerfile in docker/ruby/Dockerfile Dockerfile.production; do
+	if ! grep -q "^ARG NODE_VERSION=$NODE_VERSION" $dockerfile; then
+		echo "$dockerfile has wrong node: $(grep '^ARG NODE_MAJOR' $dockerfile)" >&2
+		EXIT=1
+	fi
+
+	if ! grep -q "^ARG RUBY_VERSION=$RUBY_VERSION" $dockerfile; then
+		echo "$dockerfile has wrong ruby: $(grep '^FROM ruby:' $dockerfile)" >&2
+		EXIT=1
+	fi
+done
+
+exit $EXIT
