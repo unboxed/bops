9351a687dfe8e02b1b5a162a78af829a19005c0e
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2025-04-22 11:02:31 +0100

    Fix invalid syntax in ts_query

--- app/models/local_authority/policy_reference.rb
+++ app/models/local_authority/policy_reference.rb
@@ -44,7 +44,7 @@ class LocalAuthority < ApplicationRecord
 
       def search_param(query)
         query.to_s
-          .scan(/[-\w]{1,}/)
+          .scan(/-?[\w][-\w]*/)
           .map { |word| word.gsub(/^-/, "!") }
           .map { |word| word.gsub(/-$/, "") }
           .map { |word| word.gsub(/.+/, "\\0:*") }
