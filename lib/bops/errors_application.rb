f6f502f05a544ff8e646bf7fb3626abfd6c6b023
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2025-05-20 14:17:35 +0100

    Add request.uuid to error pages too

--- lib/bops/errors_application.rb
+++ lib/bops/errors_application.rb
@@ -2,7 +2,14 @@
 
 module Bops
   class ErrorsApplication < ActionDispatch::PublicExceptions
+    def call(env)
+      @request = ActionDispatch::Request.new(env)
+      super
+    end
+
     private
+
+    attr_reader :request
 
     STATUSES = {
       404 => :not_found,
@@ -11,7 +18,7 @@ module Bops
 
     def render(status, content_type, body)
       status_name = STATUSES[status]
-      response_body = ErrorsController.render status_name, layout: nil
+      response_body = ErrorsController.render status_name, layout: nil, locals: {request_id: request.uuid}
       [status, {Rack::CONTENT_TYPE => "#{content_type}; charset=#{ActionDispatch::Response.default_charset}",
                 Rack::CONTENT_LENGTH => response_body.bytesize.to_s}, [response_body]]
     rescue
