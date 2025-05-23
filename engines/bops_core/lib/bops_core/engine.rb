23c79bb9e1ba4290c2a4379223df4db2be4216e3
Andrew White <andrew.white@unboxed.co>
2025-05-23 06:28:11 +0100

    Add BOPS-flavoured error pages

--- engines/bops_core/lib/bops_core/engine.rb
+++ engines/bops_core/lib/bops_core/engine.rb
@@ -5,5 +5,21 @@ require "govuk/components"
 module BopsCore
   class Engine < ::Rails::Engine
     isolate_namespace BopsCore
+
+    initializer "bops_core.rescue_responses" do
+      config.after_initialize do
+        ActionDispatch::ExceptionWrapper.rescue_responses.merge! \
+          "BopsCore::Errors::ClientError" => :bad_request,
+          "BopsCore::Errors::BadRequestError" => :bad_request,
+          "BopsCore::Errors::UnauthorizedError" => :unauthorized,
+          "BopsCore::Errors::ForbiddenError" => :forbidden,
+          "BopsCore::Errors::NotFoundError" => :not_found,
+          "BopsCore::Errors::NotAcceptableError" => :not_acceptable,
+          "BopsCore::Errors::UnprocessableContentError" => :unprocessable_content,
+          "BopsCore::Errors::ServerError" => :internal_server_error,
+          "BopsCore::Errors::InternalServerError" => :internal_server_error,
+          "BopsCore::Errors::ServiceUnavailableError" => :service_unavailable
+      end
+    end
   end
 end
