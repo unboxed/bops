622f8a04c0e3a6621dcecfbcbb8ed7ab26305106
Evangelos Giataganas <e.giataganas@gmail.com>
2025-06-13 23:15:02 +0300

    Drop application_types records where local_authority was nil

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_13_200838) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_13_210912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "plpgsql"
