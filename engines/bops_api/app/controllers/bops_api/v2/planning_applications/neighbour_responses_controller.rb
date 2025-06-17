815806617bffdbaf4e0c19e51012ccd40927c536
gabimartini <gabimartinii@hotmail.com>
2025-06-17 11:26:20 +0100

    fix lint

--- engines/bops_api/app/controllers/bops_api/v2/planning_applications/neighbour_responses_controller.rb
+++ engines/bops_api/app/controllers/bops_api/v2/planning_applications/neighbour_responses_controller.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 module BopsApi
-    module V2
-        module PlanningApplications
-            class NeighbourResponsesController < AuthenticatedController
-    def create
-      @planning_application = find_planning_application(params[:planning_application_id])
-      @consultation = @planning_application.consultation
+  module V2
+    module PlanningApplications
+      class NeighbourResponsesController < AuthenticatedController
+        def create
+          @planning_application = find_planning_application(params[:planning_application_id])
+          @consultation = @planning_application.consultation
 
-      @neighbour_response = NeighbourResponseCreationService.new(
-        params:,
-        planning_application: @planning_application
-      ).call
+          @neighbour_response = NeighbourResponseCreationService.new(
+            params:,
+            planning_application: @planning_application
+          ).call
 
-      render json: {message: "Neighbour response created successfully"}, status: :created
-    rescue NeighbourResponseCreationService::CreateError => e
-      render json: {error: e.message}, status: :unprocessable_entity
-    end
-            end
+          render json: {message: "Neighbour response created successfully"}, status: :created
+        rescue NeighbourResponseCreationService::CreateError => e
+          render json: {error: e.message}, status: :unprocessable_entity
         end
+      end
     end
+  end
 end
