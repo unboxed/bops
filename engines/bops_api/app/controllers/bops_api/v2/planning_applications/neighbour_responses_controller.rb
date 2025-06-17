4bcc19e70dded48c8f638574ea5888916381dd3f
gabimartini <gabimartinii@hotmail.com>
2025-06-17 11:16:04 +0100

    fix lint

--- engines/bops_api/app/controllers/bops_api/v2/planning_applications/neighbour_responses_controller.rb
+++ engines/bops_api/app/controllers/bops_api/v2/planning_applications/neighbour_responses_controller.rb
@@ -18,30 +18,7 @@ module BopsApi
       render json: {error: e.message}, status: :unprocessable_entity
     end
 
-              private
-
-    def find_neighbour
-        @consultation.neighbours.find_by(address: neighbour_response_params[:address])
-    end
-
-    def validate_required_params!(*required_keys)
-      missing_keys = required_keys.select { |key| neighbour_response_params[key].blank? }
-      if missing_keys.any?
-        raise BopsApi::Errors::InvalidRequestError, "#{missing_keys.join(", ").humanize} #{"is".pluralize(missing_keys.size)} required"
-      end
-    end
-
-    def neighbour_response_params
-        params.permit(
-          :name, :address, :response, :summary_tag, :tags, :email, tags: []
-        )
-    end
-
-    def set_error_messages
-      flash.now[:alert] = @neighbour_response.neighbour.errors.full_messages.join("\n") if @neighbour_response.neighbour&.errors&.any?
-    end
-            end
         end
     end
 end
-# end
+end
