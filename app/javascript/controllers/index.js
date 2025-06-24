2d800fceb733075cc679a208c79a549cbd824c26
Andrew White <andrew.white@unboxed.co>
2025-06-24 04:33:27 +0100

    Add summary stimulus controller
    This shows the first couple of lines from the hidden content.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -103,6 +103,9 @@ import SubmitFormController from "./submit_form_controller.js"
 
 application.register("submit-form", SubmitFormController)
 
+import SummaryController from "@/bops_core/controllers/summary_controller.js"
+application.register("summary", SummaryController)
+
 import TaskAccordionController from "@/bops_core/controllers/task_accordion_controller.js"
 
 application.register("task-accordion", TaskAccordionController)
