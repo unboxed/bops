9d6b71715f78e63a6619274633afed8b4a9c8be6
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2025-07-01 10:43:37 +0100

    Add Caseable delegated type

--- app/models/case_record.rb
+++ app/models/case_record.rb
@@ -3,6 +3,8 @@
 require "securerandom"
 
 class CaseRecord < ApplicationRecord
+  delegated_type :caseable, types: %w[Enforcement], dependent: :destroy
+
   belongs_to :local_authority
 
   after_initialize :generate_uuid
--- app/models/enforcement.rb
+++ app/models/enforcement.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class Enforcement < ApplicationRecord
+  include Caseable
 end
