79778b3bf40766799b799c8a3683c6bb67fda4e8
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2025-07-11 10:10:42 +0100

    Align Enforcement column names with PlanningApplication

--- spec/factories/enforcement.rb
+++ spec/factories/enforcement.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :enforcement do
     association :case_record, strategy: :build
-    address_line_1 { Faker::Address.street_address }
-    address_line_2 { Faker::Address.secondary_address }
+    address_1 { Faker::Address.street_address }
+    address_2 { Faker::Address.secondary_address }
     town { Faker::Address.city }
     county { Faker::Address.state }
     postcode { Faker::Address.postcode }
