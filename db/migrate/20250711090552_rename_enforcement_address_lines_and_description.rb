efbfdb67cdcc404817d38e05edc5fad4fee773d8
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2025-07-11 10:10:42 +0100

    Align Enforcement column names with PlanningApplication

--- db/migrate/20250711090552_rename_enforcement_address_lines_and_description.rb
+++ db/migrate/20250711090552_rename_enforcement_address_lines_and_description.rb
@@ -2,7 +2,7 @@
 
 class RenameEnforcementAddressLinesAndDescription < ActiveRecord::Migration[7.2]
   def change
-    safety_assured do
+    safety_assured do # treating this as safe because these are not used yet
       rename_column :enforcements, :address_line_1, :address_1
       rename_column :enforcements, :address_line_2, :address_2
       rename_column :enforcements, :breach_description, :description
