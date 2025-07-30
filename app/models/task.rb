76c8e4fac39a3ef4e4faaed48997cdef769ea924
Andrew White <andrew.white@unboxed.co>
2025-07-30 10:17:44 +0100

    Don't use deprecated form of enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,19 +1,14 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  belongs_to :parent, polymorphic: true
+  enum :status, %i[not_started in_progress completed].index_with(&:to_s)
 
+  belongs_to :parent, polymorphic: true
   has_many :tasks, -> { order(:position) }, as: :parent, dependent: :destroy, autosave: true
 
   before_save :set_slug
 
   validates :slug, :name, presence: true
-
-  enum status: {
-    not_started: "not_started",
-    in_progress: "in_progress",
-    completed: "completed"
-  }
 
   def full_slug
     @full_slug ||= parent.is_a?(Task) ? "#{parent.full_slug}/#{slug}" : slug
