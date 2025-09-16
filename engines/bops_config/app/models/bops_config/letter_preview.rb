ad527feea254ba12ab7122b2e43bc27fa6b698d7
Rhian Lewis <rhian.lewis@unboxedconsulting.com>
2025-09-16 09:24:02 +0100

    Move Notify models into concern

--- engines/bops_config/app/models/bops_config/letter_preview.rb
+++ engines/bops_config/app/models/bops_config/letter_preview.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module BopsAdmin
+module BopsConfig
   class LetterPreview
     include BopsCore::LetterPreview
   end
--- engines/bops_config/app/models/letter_preview.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class LetterPreview < BopsAdmin::LetterPreview
-end
