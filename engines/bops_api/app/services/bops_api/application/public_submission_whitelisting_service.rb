e8517204ee232c04ec34c68a29b8d196051b55a4
Rebecca O'Neill <rebecca.e.oneill3@gmail.com>
2025-09-19 16:55:12 +0100

    Add whitelisted fields to public submission endpoint

--- engines/bops_api/app/services/bops_api/application/public_submission_whitelisting_service.rb
+++ engines/bops_api/app/services/bops_api/application/public_submission_whitelisting_service.rb
@@ -7,35 +7,42 @@ module BopsApi
 
       def initialize(planning_application:)
         @planning_application = planning_application
+        @submission = planning_application.params_v2
       end
 
       def call
-        return unless (submission = planning_application.params_v2)
+        return unless submission
 
-        filter_submission(FILTER, submission)
+        result = filter_submission(FILTER, submission)
+
+        result[:files] = whitelisted_files if submission["files"]
+        result
       rescue => e
         raise WhitelistingError, e.message
       end
 
       FILTER = {
         data: {
-          application: {
-            type: %i[value description]
-          },
-          applicant: {
-            name: %i[first last]
-          },
-          property: {
-            address: %i[singleline title street postcode town]
-          },
-          proposal: %i[description]
-        },
-        metadata: %i[id source]
+          application: %i[type declaration planningApp leadDeveloper vacantBuildingCredit],
+          applicant: %i[name type address agent],
+          property: %i[
+            address boundary flood planning localAuthorityDistrict
+            region trees type units ward occupation ownership parking
+            socialLandlord titleNumber EPC
+          ],
+          proposal: %i[
+            access boundary charging cost date description
+            ecology energy extend flood greenRoof new newBuildings newDwellings
+            newStoreys parking projectType structures units urbanGreeningFactor
+            utilities waste water
+          ],
+          user: %i[role]
+        }
       }
 
       private
 
-      attr_reader :planning_application
+      attr_reader :planning_application, :submission
 
       def filter_submission(filter, source, destination = nil)
         if destination.nil?
@@ -55,6 +62,28 @@ module BopsApi
         end
 
         destination
+      end
+
+      def submission_file_names
+        submission["files"].map do |file|
+          URI.decode_uri_component(File.basename(URI.parse(file["name"]).path))
+        end
+      end
+
+      def whitelisted_files
+        planning_application.documents.map do |document|
+          filename = document.file.filename.to_s
+          if document.publishable? && submission_file_names.include?(filename)
+            {
+              name: filename,
+              type: document.tags.map { |tag| {"value" => tag, "description" => tag.humanize} }
+            }
+          else
+            {
+              name: "Unpublished document - sensitive"
+            }
+          end
+        end
       end
     end
   end
