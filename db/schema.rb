64146a9b60c99bd469a84af41c5fb5d85863d343
Andrew White <andrew.white@unboxed.co>
2025-10-18 06:33:09 +0100

    Update schema.rb with new plpgsql extension

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[8.0].define(version: 2025_10_06_174502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "postgis"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
