1ea325f3ea329739b132e55c687f95a548455573
Ben Baumann <34001723+benbaumann95@users.noreply.github.com>
2025-10-27 16:14:58 +0000

    Add support for ODP schema v0.7.6

--- spec/system/planning_applications/replacement_document_validation_request_spec.rb
+++ spec/system/planning_applications/replacement_document_validation_request_spec.rb
@@ -212,7 +212,7 @@ RSpec.describe "Requesting document changes to a planning application", type: :s
       end
 
       click_link "Supporting documents"
-      click_link "Show all (73)"
+      click_link "Show all (74)"
 
       check "Sustainability statement"
 
