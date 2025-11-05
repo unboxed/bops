0b34e8c6090fc2880e022c1ed653de826d50fd2c
Benjamin Eskola <ben.eskola@unboxed.co>
2025-11-05 13:52:50 +0000

    Remove unnecessary route

--- engines/bops_preapps/config/routes.rb
+++ engines/bops_preapps/config/routes.rb
@@ -7,10 +7,6 @@ BopsPreapps::Engine.routes.draw do
   get "/planning_applications", to: "pre_applications#index"
 
   scope "/cases/:reference" do
-    resources :assign_users, only: %i[index] do
-      patch :update, on: :collection
-    end
-
     get "/*slug/edit", to: "tasks#edit", as: :edit_task
     patch "/*slug", to: "tasks#update"
     get "/*slug", to: "tasks#show", as: :task
