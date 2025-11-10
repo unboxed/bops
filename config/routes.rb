937d120ac870fc718078db12dbef297181f2d376
Benjamin Eskola <ben.eskola@unboxed.co>
2025-11-10 11:16:10 +0000

    Remove placeholder site-description route
    This was made unnecessary by 8c2610ba1 (#2654) and should have been removed then

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,6 @@ Rails.application.routes.draw do
     "https://oasis-marsupial-465.notion.site/Guide-to-GOV-UK-Notify-set-up-7c18a8f3d43444d098c1f79eab48016c"
   end
 
-  direct :planning_application_assessment_site_description do |pa|
-    pa.site_description.present? ?
-      [:edit, pa, :assessment, pa.site_description, category: :site_description] :
-      [:new, pa, :assessment, :assessment_detail, category: :site_description]
-  end
-
   direct :planning_application_assessment_summary_of_advice do |pa|
     pa.summary_of_advice.present? ?
       [:edit, pa, :assessment, pa.summary_of_advice, category: :site_description] :
