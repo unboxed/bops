5f5186b1e3d304554047f5532e28f43842909810
Benjamin Eskola <ben.eskola@unboxed.co>
2025-11-17 11:37:04 +0000

    Add preapp assessment task for considerations and advice

--- engines/bops_core/app/helpers/bops_core/application_helper.rb
+++ engines/bops_core/app/helpers/bops_core/application_helper.rb
@@ -121,5 +121,23 @@ module BopsCore
     def show_map_pin?(caseable, data)
       (data[:geojson].blank? || data[:invalid_red_line_boundary].present?) && caseable.lonlat.present?
     end
+
+    def summary_tag_label(summary_tag)
+      return unless summary_tag
+      label = t("helpers.summary_tags.#{summary_tag}", default: summary_tag.titleize)
+      css_class =
+        case summary_tag
+        when "complies"
+          "govuk-tag govuk-tag--green"
+        when "needs_changes"
+          "govuk-tag govuk-tag--yellow"
+        when "does_not_comply"
+          "govuk-tag govuk-tag--red"
+        else
+          "govuk-tag govuk-tag--grey"
+        end
+
+      content_tag(:span, label, class: css_class)
+    end
   end
 end
