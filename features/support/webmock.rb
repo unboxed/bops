5a25c84664ffd3c0ce68dc6583bd0e7521620d8f
Benjamin Eskola <ben.eskola@unboxed.co>
2025-11-20 09:27:08 +0000

    Remove cucumber
    I'm not sure we've ever caught actual errors this way and we haven't
    been adding new tests for any new features in several years: the only
    changes made have been to fix existing pages when the design changes.

--- features/support/webmock.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require "webmock/cucumber"
-
-allowed_hosts = ["chromedriver.storage.googleapis.com"]
-WebMock.disable_net_connect! allow_localhost: true, allow: allowed_hosts
