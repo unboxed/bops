b48279276b7145c009794c818e4dcc3056a3a0aa
Benjamin Eskola <ben.eskola@unboxed.co>
2025-11-25 14:50:41 +0000

    Add separate save button to site visit task

--- engines/bops_preapps/app/forms/bops_preapps/tasks/site_visit_form.rb
+++ engines/bops_preapps/app/forms/bops_preapps/tasks/site_visit_form.rb
@@ -9,23 +9,36 @@ module BopsPreapps
         super
 
         @site_visits = @planning_application.site_visits.by_created_at_desc.includes(:created_by)
+        @is_submitting_task = false
       end
 
       def update(params)
-        site_visit = @planning_application.site_visits.new
+        if @is_submitting_task
+          begin
+            site_visit = @planning_application.site_visits.new
 
-        ActiveRecord::Base.transaction do
-          site_visit.update!(params)
-          task.update!(status: :completed)
+            ActiveRecord::Base.transaction do
+              site_visit.update!(params)
+              task.update!(status: :in_progress)
+            end
+          rescue ActiveRecord::RecordInvalid
+            false
+          end
+        elsif params[:button] == "save_draft" && !@task.completed?
+          task.update(status: :in_progress)
+        else
+          task.update(status: :completed)
         end
-      rescue ActiveRecord::RecordInvalid
-        false
       end
 
       def permitted_fields(params)
-        params.require(:task)
-          .permit(:decision, :comment, :visited_at, :neighbour_id, :address, documents: [])
-          .merge(created_by: Current.user)
+        if (@is_submitting_task = params[:task].present?)
+          params.require(:task)
+            .permit(:decision, :comment, :visited_at, :neighbour_id, :address, documents: [])
+            .merge(created_by: Current.user)
+        else
+          params
+        end
       end
     end
   end
