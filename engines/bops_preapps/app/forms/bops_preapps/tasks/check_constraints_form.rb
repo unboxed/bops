18a64f8051b6d4f9aa97ee6f97470f5e0a903d6e
Benjamin Eskola <ben.eskola@unboxed.co>
2025-12-04 10:42:01 +0000

    Add Check Constraints task

---
+++ engines/bops_preapps/app/forms/bops_preapps/tasks/check_constraints_form.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+module BopsPreapps
+  module Tasks
+    class CheckConstraintsForm < Form
+      self.task_actions = %w[add_constraint remove_constraint save_draft save_and_complete]
+
+      attribute :constraint_id, :integer
+
+      delegate :planning_application_constraints, to: :planning_application
+
+      def update(params)
+        super do
+          case action
+          when "add_constraint"
+            add_constraint
+          when "remove_constraint"
+            remove_constraint
+          when "save_draft"
+            task.start!
+          when "save_and_complete"
+            task.complete!
+          else
+            raise ArgumentError, "Invalid task action: #{action.inspect}"
+          end
+        end
+      end
+
+      def other_constraints(search_param:)
+        Constraint.other_constraints(search_param, planning_application)
+      end
+
+      def form_params(params)
+        params.fetch(param_key, {}).permit(:constraint_id)
+      end
+
+      def flash(type, controller)
+        case action
+        when "add_constraint", "remove_constraint"
+          case type
+          when :notice
+            controller.t(".#{slug}.#{action}.success")
+          when :alert
+            controller.t(".#{slug}.#{action}.failure")
+          end
+        else
+          super
+        end
+      end
+
+      private
+
+      def add_constraint
+        transaction do
+          planning_application_constraints.new(constraint_id:, identified_by: Current.user.name).save! && task.start!
+        end
+      end
+
+      def remove_constraint
+        transaction do
+          planning_application_constraints.find(constraint_id).delete && task.start!
+        end
+      end
+    end
+  end
+end
