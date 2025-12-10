76fa782f0fbf0ca40ac260b95f6efd8424ac6cba
Rebecca O'Neill <rebecca.e.oneill3@gmail.com>
2025-12-10 16:14:14 +0000

    Add check description task setup

---
+++ engines/bops_preapps/app/forms/bops_preapps/tasks/check_description_form.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module BopsPreapps
+  module Tasks
+    class CheckDescriptionForm < Form
+      self.task_actions = %w[save_and_complete]
+
+      attribute :valid_description, :boolean
+
+      with_options on: :save_and_complete do
+        validates :valid_description, inclusion: {in: [true, false], message: "Select whether the description is correct"}
+      end
+
+      after_initialize do
+        self.valid_description = planning_application.valid_description
+      end
+
+      def update(params)
+        super do
+          case action
+          when "save_and_complete"
+            save_and_complete
+          else
+            raise ArgumentError, "Invalid task action: #{action.inspect}"
+          end
+        end
+      end
+
+      def redirect_url(options = {})
+        if valid_description
+          super
+        else
+          Rails.application.routes.url_helpers.new_planning_application_validation_validation_request_path(
+            planning_application,
+            type: "description_change"
+          )
+        end
+      end
+
+      private
+
+      def save_and_complete
+        transaction do
+          planning_application.update!(valid_description:)
+          valid_description ? task.complete! : task.in_progress!
+        end
+      end
+    end
+  end
+end
