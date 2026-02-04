aee6fef65e1b5e51ba19f430f8f5f681e903528a
Benjamin Eskola <ben.eskola@unboxed.co>
2026-02-04 16:27:21 +0000

    Remove obsolete method
    This seems to have been left behind 5 years ago when removing pundit,
    then mistakenly copied to the other controller later.

--- engines/bops_preapps/app/controllers/bops_preapps/authentication_controller.rb
+++ engines/bops_preapps/app/controllers/bops_preapps/authentication_controller.rb
@@ -4,13 +4,6 @@ module BopsPreapps
   class AuthenticationController < ApplicationController
     before_action :authenticate_user!
 
-    def user_not_authorized(_error, message = t("user_not_authorized"))
-      respond_to do |format|
-        format.html { redirect_to pundit_redirect_url, alert: message }
-        format.json { render json: [message], status: :unauthorized }
-      end
-    end
-
     private
 
     def set_planning_application
