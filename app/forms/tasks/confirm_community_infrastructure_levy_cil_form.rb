54279169852e9ad4e9d6f9845b7b0fe265095136
Rebecca O'Neill <rebecca.e.oneill3@gmail.com>
2026-02-05 15:57:43 +0000

    Add confirm CIL task all application types

---
+++ app/forms/tasks/confirm_community_infrastructure_levy_cil_form.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Tasks
+  class ConfirmCommunityInfrastructureLevyCilForm < Form
+    self.task_actions = %w[save_and_complete edit_form]
+
+    attribute :cil_liable, :boolean
+
+    with_options on: :save_and_complete do
+      validates :cil_liable, inclusion: {in: [true, false, "true", "false"], message: "Select whether the application is liable for CIL."}
+    end
+
+    def recommended_cil_liable
+      if planning_application.likely_cil_exempt?
+        false
+      elsif planning_application.likely_cil_liable?
+        true
+      end
+    end
+
+    def update(params)
+      super do
+        case action
+        when "save_and_complete" then save_and_complete
+        when "edit_form" then edit_form
+        end
+      end
+    end
+
+    private
+
+    def save_and_complete
+      transaction do
+        planning_application.update!(cil_liable: cil_liable)
+        task.completed!
+      end
+    end
+  end
+end
