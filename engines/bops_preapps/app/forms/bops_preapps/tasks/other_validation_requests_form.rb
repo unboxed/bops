035113f9150af8102c6e21319b498af8790153a8
Ben Baumann <34001723+benbaumann95@users.noreply.github.com>
2026-02-06 12:23:54 +0000

    Add other validation requests task for all application types
    Extract OtherValidationRequestsForm concern to bops_core so the task
    works for all application types
    
    - Create BopsCore::Tasks::OtherValidationRequestsForm concern
    - Move view from bops_preapps to bops_core
    - Update view to use govuk_table and govuk_inset_text components
    - Add main app form wrapper for non-preapp types
    - Update bops_preapps form to include the concern

--- engines/bops_preapps/app/forms/bops_preapps/tasks/other_validation_requests_form.rb
+++ engines/bops_preapps/app/forms/bops_preapps/tasks/other_validation_requests_form.rb
@@ -3,55 +3,7 @@
 module BopsPreapps
   module Tasks
     class OtherValidationRequestsForm < Form
-      class ValidationRequestDecorator < SimpleDelegator
-        def created_by
-          user&.name
-        end
-
-        def status
-          case state
-          when "closed"
-            :updated
-          when "cancelled"
-            :cancelled
-          else
-            :invalid
-          end
-        end
-      end
-
-      self.task_actions = %w[save_draft save_and_complete edit_form]
-
-      def update(params)
-        super do
-          case action
-          when "save_draft"
-            save_draft
-          when "save_and_complete"
-            save_and_complete
-          when "edit_form"
-            task.in_progress!
-          else
-            raise ArgumentError, "Invalid task action: #{action.inspect}"
-          end
-        end
-      end
-
-      def validation_requests
-        @validation_requests ||= validation_requests_relation.load
-      end
-
-      def each_validation_request
-        validation_requests.each do |validation_request|
-          yield ValidationRequestDecorator.new(validation_request)
-        end
-      end
-
-      private
-
-      def validation_requests_relation
-        planning_application.other_change_validation_requests.includes(:user).by_created_at
-      end
+      include BopsCore::Tasks::OtherValidationRequestsForm
     end
   end
 end
