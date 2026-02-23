4713959f64d9497649f0df087f13da303221f842
Rebecca O'Neill <rebecca.e.oneill3@gmail.com>
2026-02-23 16:26:03 +0000

    Add ability to cancel validation requests before decision

--- spec/system/tasks/check_description_spec.rb
+++ spec/system/tasks/check_description_spec.rb
@@ -42,6 +42,51 @@ RSpec.describe "Check description task", type: :system do
             "/planning_applications/#{planning_application.reference}/check-and-validate/check-application-details/check-description"
           )
       end
+
+      context "when an existing description change request" do
+        let(:validation_requests) do
+          create(:validation_request, planning_application:, type: "DescriptionChangeValidationRequest", user: user)
+        end
+
+        it "allows description change request to be edited and deleted", capybara: true do
+          within ".bops-sidebar" do
+            click_link "Check description"
+          end
+
+          choose "No"
+          choose "No, update description immediately"
+          fill_in "Enter an amended description", with: "This is an updated description."
+          click_button "Save and mark as complete"
+
+          expect(page).to have_content("Description check was successfully saved")
+          expect(page).to have_content("Description change")
+
+          expect(task.reload).to be_completed
+          visit("/planning_applications/#{planning_application.reference}/check-and-validate/check-application-details/check-description")
+          expect(page).to have_content("Description change request")
+          click_link "Request a new description change"
+          fill_in "Enter an amended description", with: "This is an updated description. Again."
+
+          choose "Yes, applicant agreement needed"
+          click_button "Update description"
+
+          expect(page).to have_content("Description change request successfully sent.")
+
+          within ".bops-sidebar" do
+            click_link "Check description"
+          end
+
+          expect(page).to have_content("This is an updated description. Again.")
+
+          click_link("Cancel description change request")
+          fill_in "Explain to the applicant why this request is being cancelled", with: "No longer needed."
+
+          click_button "Confirm cancellation"
+          expect(page).to have_content("Description change request successfully cancelled")
+
+          expect(planning_application.validation_requests.last.state).to eq("cancelled")
+        end
+      end
     end
   end
 end
