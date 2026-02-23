c4349c6cc28f804334192538de1858bc242ad1df
Benjamin Eskola <ben.eskola@unboxed.co>
2026-02-23 10:05:34 +0000

    Redirect to first valdation task for all application types

--- app/controllers/planning_applications/validation/tasks_controller.rb
+++ app/controllers/planning_applications/validation/tasks_controller.rb
@@ -4,11 +4,14 @@ module PlanningApplications
   module Validation
     class TasksController < BaseController
       before_action :set_items_counter, only: :index
-      before_action :redirect_to_initial_task
 
       def index
-        respond_to do |format|
-          format.html
+        task = @planning_application.case_record.tasks.find_by!(section: "Validation").first_child
+
+        if @planning_application.pre_application?
+          redirect_to BopsPreapps::Engine.routes.url_helpers.task_path(@planning_application, task)
+        else
+          redirect_to task_path(@planning_application, task)
         end
       end
 
@@ -20,14 +23,6 @@ module PlanningApplications
 
       def set_items_counter
         @items_counter = @planning_application.items_counter
-      end
-
-      def redirect_to_initial_task
-        task = @planning_application.case_record.tasks.find_by(section: "Validation")&.first_child
-
-        return unless task
-
-        redirect_to BopsPreapps::Engine.routes.url_helpers.task_path(@planning_application, task) if @planning_application.pre_application?
       end
     end
   end
