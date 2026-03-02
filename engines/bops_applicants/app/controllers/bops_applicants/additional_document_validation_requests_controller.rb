5bc67e70c6ad8eabb3e4a3cc85a8154b67fec59e
Andrew White <andrew.white@unboxed.co>
2026-03-02 09:00:00 +0000

    Show an error when uploading an invalid document type
    Despite specifying the valid content types on the upload field they can be
    bypassed so show a more informative error when they're detected on the server.

--- engines/bops_applicants/app/controllers/bops_applicants/additional_document_validation_requests_controller.rb
+++ engines/bops_applicants/app/controllers/bops_applicants/additional_document_validation_requests_controller.rb
@@ -3,11 +3,28 @@
 module BopsApplicants
   class AdditionalDocumentValidationRequestsController < ValidationRequestsController
     before_action :set_documents, only: %i[show]
+    before_action :validate_additional_documents, only: %i[update]
 
     private
 
     def set_documents
       @documents = @validation_request.additional_documents
+    end
+
+    def additional_documents_url
+      if @validation_request.open?
+        edit_additional_document_validation_request_path(@validation_request.id, access_control_params)
+      else
+        additional_document_validation_request_path(@validation_request.id, access_control_params)
+      end
+    end
+
+    def validate_additional_documents
+      uploaded_files.each do |file|
+        unless permitted_content_type?(file)
+          redirect_to additional_documents_url, alert: "Only JPEG, PNG or PDF file types are supported" and break
+        end
+      end
     end
 
     def validation_request_params
--- engines/bops_applicants/app/controllers/bops_applicants/replacement_document_validation_requests_controller.rb
+++ engines/bops_applicants/app/controllers/bops_applicants/replacement_document_validation_requests_controller.rb
@@ -3,12 +3,29 @@
 module BopsApplicants
   class ReplacementDocumentValidationRequestsController < ValidationRequestsController
     before_action :set_documents, only: %i[show]
+    before_action :validate_replacement_document, only: %i[update]
 
     private
 
     def set_documents
       @old_document = @validation_request.old_document
       @new_document = @validation_request.new_document
+    end
+
+    def replacement_document_url
+      if @validation_request.open?
+        edit_replacement_document_validation_request_path(@validation_request.id, access_control_params)
+      else
+        replacement_document_validation_request_path(@validation_request.id, access_control_params)
+      end
+    end
+
+    def validate_replacement_document
+      return if replacement_file.blank?
+
+      unless permitted_content_type?(replacement_file)
+        redirect_to replacement_document_url, alert: "Only JPEG, PNG or PDF file types are supported" and return
+      end
     end
 
     def validation_request_params
