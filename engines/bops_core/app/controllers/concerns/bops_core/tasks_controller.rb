65a049c7c9e7f8e8df21b3f6171bf48d9e144061
Rebecca O'Neill <rebecca.e.oneill3@gmail.com>
2026-04-07 16:07:40 +0100

    Add site notice combined task

--- engines/bops_core/app/controllers/concerns/bops_core/tasks_controller.rb
+++ engines/bops_core/app/controllers/concerns/bops_core/tasks_controller.rb
@@ -92,7 +92,7 @@ module BopsCore
     end
 
     def failure_template
-      return :edit if params[:task_action].in?(%w[update_site_visit update_condition confirm_publication])
+      return :edit if params[:task_action].in?(%w[update_site_visit update_condition confirm_publication confirm_display])
 
       :show
     end
