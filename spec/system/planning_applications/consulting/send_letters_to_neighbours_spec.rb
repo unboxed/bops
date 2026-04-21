997d47f091a7798fa8d1b6090bcc22cce3942c61
Benjamin Eskola <ben.eskola@unboxed.co>
2026-04-21 13:06:07 +0100

    Fix duplication of neighbour letter resend reason

--- spec/system/planning_applications/consulting/send_letters_to_neighbours_spec.rb
+++ spec/system/planning_applications/consulting/send_letters_to_neighbours_spec.rb
@@ -253,7 +253,8 @@ RSpec.describe "Send letters to neighbours", :js, show_sidebar: false, type: :sy
   end
 
   context "when letters have already been sent" do
-    let(:neighbour) { create(:neighbour, address: "1, Test Lane, E1 5AT", consultation:) }
+    let(:neighbour1) { create(:neighbour, address: "1, Test Lane, E1 5AT", consultation:) }
+    let(:neighbour2) { create(:neighbour, address: "3, Test Lane, E1 5AT", consultation:) }
 
     before do
       travel_to(2.weeks.ago) do
@@ -262,9 +263,12 @@ RSpec.describe "Send letters to neighbours", :js, show_sidebar: false, type: :sy
 
       sign_in assessor
 
-      neighbour_letter = create(:neighbour_letter, neighbour:, status: "submitted", notify_id: "123")
-      neighbour.touch(:last_letter_sent_at)
-      stub_get_notify_status(notify_id: neighbour_letter.notify_id)
+      neighbour_letter1 = create(:neighbour_letter, neighbour: neighbour1, status: "submitted", notify_id: "123")
+      neighbour_letter2 = create(:neighbour_letter, neighbour: neighbour2, status: "submitted", notify_id: "456")
+      neighbour1.touch(:last_letter_sent_at)
+      neighbour2.touch(:last_letter_sent_at)
+      stub_get_notify_status(notify_id: neighbour_letter1.notify_id)
+      stub_get_notify_status(notify_id: neighbour_letter2.notify_id)
     end
 
     it "shows that letters have been sent" do
@@ -276,7 +280,7 @@ RSpec.describe "Send letters to neighbours", :js, show_sidebar: false, type: :sy
 
       click_link "Send letters to neighbours"
       within("#selected-neighbours-list") do
-        expect(page).to have_content(neighbour.address)
+        expect(page).to have_content(neighbour1.address)
       end
     end
 
@@ -295,7 +299,8 @@ RSpec.describe "Send letters to neighbours", :js, show_sidebar: false, type: :sy
       expect(page).to have_current_path("/planning_applications/#{reference}/consultation/neighbour_letters")
       expect(page).to have_content("Letters have been sent to neighbours")
 
-      expect(neighbour.neighbour_letters.length).to eq(2)
+      expect(neighbour1.neighbour_letters.length).to eq(2)
+      expect(neighbour2.neighbour_letters.length).to eq(2)
       expect(consultation.reload.end_date).to be_after(orig_deadline)
     end
 
@@ -305,7 +310,8 @@ RSpec.describe "Send letters to neighbours", :js, show_sidebar: false, type: :sy
       click_link "Send letters to neighbours"
 
       click_button "Confirm and send letters"
-      expect(neighbour.neighbour_letters.length).to eq(1)
+      expect(neighbour1.neighbour_letters.length).to eq(1)
+      expect(neighbour2.neighbour_letters.length).to eq(1)
     end
 
     it "requires a reason to resend letters" do
@@ -317,7 +323,7 @@ RSpec.describe "Send letters to neighbours", :js, show_sidebar: false, type: :sy
 
       click_button "Confirm and send letters"
       expect(page).to have_content "Provide a reason when resending letters to previously contacted neighbours"
-      expect(neighbour.neighbour_letters.length).to eq(1)
+      expect(neighbour1.neighbour_letters.length).to eq(1)
     end
 
     it "includes a reason in the letter when resending letters" do
@@ -329,17 +335,20 @@ RSpec.describe "Send letters to neighbours", :js, show_sidebar: false, type: :sy
       fill_in("Resend reason",
         with: "Previous letter mistakenly listed applicant's address as Buckingham Palace.")
 
-      expect_any_instance_of(Notifications::Client).to receive(:send_letter).with(template_id: anything,
-        personalisation: hash_including(message: match_regex(/# Application updated\nThis application has been updated. Reason: Previous letter mistakenly listed applicant's address as Buckingham Palace.\n\n# The Town and Country Planning \(General Permitted Development\) \(England\) Order 2015 Part 1, Class A/))).and_call_original
+      expect_any_instance_of(Notifications::Client).to receive(:send_letter).twice.with(template_id: anything,
+        personalisation: hash_including(message: match_regex(/\A# Application updated\nThis application has been updated. Reason: Previous letter mistakenly listed applicant's address as Buckingham Palace.\n\n# The Town and Country Planning \(General Permitted Development\) \(England\) Order 2015 Part 1, Class A/))).and_call_original
 
       click_button "Confirm and send letters"
       expect(page).to have_current_path("/planning_applications/#{reference}/consultation/neighbour_letters")
       expect(page).to have_content("Letters have been sent to neighbours")
+
+      expect(neighbour1.last_letter.text).to match_regex(/\A# Application updated\nThis application has been updated. Reason: Previous letter mistakenly listed applicant's address as Buckingham Palace.\n\n# The Town and Country Planning \(General Permitted Development\) \(England\) Order 2015 Part 1, Class A/)
+      expect(neighbour2.last_letter.text).to match_regex(/\A# Application updated\nThis application has been updated. Reason: Previous letter mistakenly listed applicant's address as Buckingham Palace.\n\n# The Town and Country Planning \(General Permitted Development\) \(England\) Order 2015 Part 1, Class A/)
     end
 
     context "when the neighbour sent a comment" do
       before do
-        neighbour.update!(source: "sent_comment")
+        neighbour1.update!(source: "sent_comment")
       end
 
       it "allows setting a reason when resend letters" do
@@ -351,7 +360,7 @@ RSpec.describe "Send letters to neighbours", :js, show_sidebar: false, type: :sy
         fill_in("Resend reason",
           with: "Previous letter mistakenly listed applicant's address as Buckingham Palace.")
 
-        expect_any_instance_of(Notifications::Client).to receive(:send_letter).with(template_id: anything,
+        expect_any_instance_of(Notifications::Client).to receive(:send_letter).twice.with(template_id: anything,
           personalisation: hash_including(message: match_regex(/# Application updated\nThis application has been updated. Reason: Previous letter mistakenly listed applicant's address as Buckingham Palace.\n\n# The Town and Country Planning \(General Permitted Development\) \(England\) Order 2015 Part 1, Class A/))).and_call_original
 
         click_button "Confirm and send letters"
