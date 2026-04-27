8d46b7cf04a5c38e3c0d40d859e607763e699826
Benjamin Eskola <ben.eskola@unboxed.co>
2026-04-27 15:05:08 +0100

    Show specific error message for unresolved post-validation requests

--- app/forms/tasks/review_and_submit_recommendation_form.rb
+++ app/forms/tasks/review_and_submit_recommendation_form.rb
@@ -7,6 +7,11 @@ module Tasks
     private
 
     def save_and_complete
+      if planning_application.open_post_validation_requests?
+        errors.add :base, :invalid, message: "All post-validation requests must be resolved before submitting"
+        return false
+      end
+
       super do
         if planning_application.can_submit_recommendation?
           planning_application.submit_recommendation!
