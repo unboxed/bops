6d8a971abcd5e742e199f70e39f4b9b247b286f7
Benjamin Eskola <ben.eskola@unboxedconsulting.com>
2026-09-08 16:29:07 +0100

    Add enforcement updated panel (audits)

---
+++ engines/bops_enforcements/app/components/bops_enforcements/updated_panel_component.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module BopsEnforcements
+  class UpdatedPanelComponent < ViewComponent::Base
+    def initialize(audits:, search:, tab_route:)
+      @audits = audits
+      @search = search
+      @tab_route = tab_route
+    end
+
+    attr_reader :audits, :search, :tab_route
+
+    def attributes
+      %i[
+        formatted_expiry_date
+        reference
+        status_tag
+        full_address
+        description
+      ]
+    end
+  end
+end
--- engines/bops_enforcements/app/helpers/bops_enforcements/application_helper.rb
+++ engines/bops_enforcements/app/helpers/bops_enforcements/application_helper.rb
@@ -4,5 +4,26 @@ module BopsEnforcements
   module ApplicationHelper
     include BopsCore::ApplicationHelper
     include BreadcrumbNavigationHelper
+
+    def activity(type_of_activity, args = nil)
+      scope = %i[audits types]
+
+      scope << :no_user if type_of_activity == "assigned" && args.blank?
+
+      t(type_of_activity, raise: true, scope:, args:)
+    rescue I18n::MissingTranslationData
+      raise ArgumentError, "Activity type: #{type_of_activity} is not valid"
+    end
+
+    def get_relevant_audit_information(audit)
+      audit.audit_comment.nil? ? audit.activity_information : audit.audit_comment
+    end
+
+    def valid_json?(comment)
+      JSON.parse(comment)
+      true
+    rescue JSON::ParserError, TypeError
+      false
+    end
   end
 end
